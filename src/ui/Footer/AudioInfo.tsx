b407d8c3edee0f6f5a779ac0f829cc453d9efc04
aungchanpyae <khantlu24@gmail.com>
2024-10-04 20:07:01 +0630

    add min-w-to prevent layout shift

--- src/ui/Footer/AudioInfo.tsx
+++ src/ui/Footer/AudioInfo.tsx
@@ -1,6 +1,6 @@
 function AudioInfo({ name }: { name: string }) {
   return (
-    <div className="[25%] max-w-[25%]">
+    <div className="min-w-[25%] max-w-[25%]">
       <div className="hidden scroll-smooth md:block md:min-w- overflow-hidden no-scrollbar hover:overflow-x-scroll ">
         <div
           className="truncate hover:text-clip  hover:w-fit"
