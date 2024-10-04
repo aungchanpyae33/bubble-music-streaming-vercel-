90ee4bc8693b07dcbb7ef9e2dc376701f8775bbd
aungchanpyae <khantlu24@gmail.com>
2024-10-04 17:51:47 +0630

    add text placeholder for overflow text

--- src/ui/Footer/AudioInfo.tsx
+++ src/ui/Footer/AudioInfo.tsx
@@ -1,7 +1,20 @@
 function AudioInfo({ name }: { name: string }) {
   return (
-    <div className="test hidden md:block md:min-w-[25%] max-w-[25%] overflow-hidden text-ellipsis">
-      {name}
+    <div className="[25%] max-w-[25%]">
+      <div className="hidden scroll-smooth md:block md:min-w- overflow-hidden no-scrollbar hover:overflow-x-scroll ">
+        <div
+          className="truncate hover:text-clip  hover:w-fit"
+          onMouseEnter={(e) => {
+            e.currentTarget.scrollIntoView({
+              behavior: "smooth",
+              inline: "end",
+            });
+          }}
+        >
+          {name}
+        </div>
+      </div>
+      <span className="text-sm">hello</span>
     </div>
   );
 }
