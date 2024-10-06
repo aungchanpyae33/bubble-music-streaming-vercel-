cca050cb62d113480da65f226d50bfd1c76f1f3e
aungchanpyae <khantlu24@gmail.com>
2024-10-06 10:57:03 +0630

    change to use animation style to show fulltext , remove focus text ,  need to calc overflow or not and style function

--- src/ui/Footer/AudioInfo.tsx
+++ src/ui/Footer/AudioInfo.tsx
@@ -1,20 +1,32 @@
+import clsx from "clsx";
+import { useState } from "react";
+
 function AudioInfo({ name }: { name: string }) {
+  const [animate, setanimatie] = useState(true);
+  console.log("render");
   return (
-    <div className="min-w-[25%] max-w-[25%]">
-      <div className="hidden scroll-smooth md:block md:min-w- overflow-hidden no-scrollbar hover:overflow-x-scroll ">
-        <div
-          className="truncate hover:text-clip  hover:w-fit"
-          onMouseEnter={(e) => {
-            e.currentTarget.scrollIntoView({
-              behavior: "smooth",
-              inline: "end",
-            });
-          }}
-        >
-          {name}
-        </div>
+    <div
+      className="hidden md:block md:w-[150px]
+    lg:w-[200px]
+    overflow-hidden"
+    >
+      {/* w-fit is needed to be get full width when animate */}
+      <div
+        className={clsx("w-fit hover:ease-linear truncate hover:text-clip", {
+          "animate-showtextoverflow": animate,
+        })}
+        // key={name}
+        onAnimationEnd={() => {
+          setanimatie(false);
+        }}
+        onMouseEnter={() => {
+          //even same anitmate value would make still twice render even though prop is not change
+          // first call is change from false to true ,seconde call is change from true to true , may be this is the reaseon
+          !animate && setanimatie(true);
+        }}
+      >
+        {name}
       </div>
-      <span className="text-sm">hello</span>
     </div>
   );
 }
