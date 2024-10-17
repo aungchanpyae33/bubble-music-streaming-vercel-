33ccc873728f1b2b4d7e946d8b02e41e2052e9ab
aungchanpyae <khantlu24@gmail.com>
2024-10-17 20:11:31 +0630

    check overflow with number px > 0 v0.1

--- src/ui/Footer/AudioInfoOverFlow.tsx
+++ src/ui/Footer/AudioInfoOverFlow.tsx
@@ -8,24 +8,25 @@ function AudioInfoOverFlow({
   name: string;
 }) {
   const [isOverFlow, animate, setanimatie] = useOverflowCheck(ofcheckDiv) as [
-    boolean,
+    number,
     boolean,
     React.Dispatch<React.SetStateAction<boolean>>
   ];
   return (
     /* w-fit is needed to be get full width when animate */
     <div
-      className={clsx("w-fit hover:ease-linear truncate hover:text-clip", {
-        "animate-showtextoverflow": animate && isOverFlow,
+      className={clsx("w-fit hover:ease-linear truncate hover:text-clip px-3", {
+        "animate-showtextoverflow": animate && isOverFlow > 0,
       })}
+      style={isOverFlow > 0 ? { animationDuration: `${isOverFlow}ms` } : {}}
       // key={name}
       onAnimationEnd={() => {
-        isOverFlow && setanimatie(false);
+        isOverFlow > 0 && setanimatie(false);
       }}
       onMouseEnter={() => {
         //even same anitmate value would make still twice render even though prop is not change
         // first call is change from false to true ,seconde call is change from true to true , may be this is the reaseon
-        !animate && isOverFlow && setanimatie(true);
+        !animate && isOverFlow > 0 && setanimatie(true);
       }}
     >
       {name}
