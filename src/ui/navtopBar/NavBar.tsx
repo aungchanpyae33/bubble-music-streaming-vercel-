b9ebbfc1ea6aa7793758120b00c1f47b43167e23
aungchanpyae3 <pyae62436@gmail.com>
2024-11-19 19:20:31 +0630

    add serch bar in layout navbar

--- src/ui/navtopBar/NavBar.tsx
+++ src/ui/navtopBar/NavBar.tsx
@@ -1,13 +1,21 @@
 import Link from "next/link";
+import SearchBar from "../searchBar/SearchBar";
+import Too from "./Too";
 
 function NavBar() {
+  console.log("nice");
   return (
-    <nav className="navBarContainer flex w-full fixed  top-0  bg-red-500 z-10 h-[50px] items-center">
+    <nav className="navBarContainer flex w-full fixed  top-0  bg-red-500 z-10 h-[50px] items-center justify-between">
       <div className="w-[70px]"></div>
-      <div className="logo flex-1 pl-2">
+      <div className="logo  px-2">
         <Link href="/">Bubble</Link>
       </div>
-      <ul className="navLinkContainer w-[40%] flex flex-wrap    justify-around">
+      <div className=" flex-1 text-end">
+        <SearchBar />
+      </div>
+
+      <ul className="navLinkContainer w-[20%] flex flex-wrap    justify-around">
+        <Too />
         <li className="bg-green-500 p-2 rounded-sm">
           <Link href={"/three"}>User</Link>
         </li>
