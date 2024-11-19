4031cedae0f4aae0d06966adce82dd02769b1205
aungchanpyae3 <pyae62436@gmail.com>
2024-11-19 19:30:05 +0630

    change logic in closeFunction  as it is not used in searchBar component and make esc key to exit in this case , and instead of adding global event , only activate when the used component open value is true

--- src/lib/CloseFunction.ts
+++ src/lib/CloseFunction.ts
@@ -1,23 +1,29 @@
-import React, { useEffect } from "react";
+import React, { RefObject, useEffect } from "react";
 
 function CloseFunctoion(
-  funvalue: boolean,
+  value: boolean,
   fun: React.Dispatch<React.SetStateAction<boolean>>,
-  TargetElement: React.MutableRefObject<HTMLDivElement | null>
+  closeElement: RefObject<HTMLButtonElement | null>
 ) {
   useEffect(() => {
-    function closeSearch(e: MouseEvent | TouchEvent) {
-      if (TargetElement.current && funvalue) {
-        // console.log("men");
-        // console.log(e.target);
-        fun(TargetElement.current?.contains(e.target as Node));
+    function closeSearch(e: KeyboardEvent) {
+      if (e.key === "Escape" && value === true) {
+        fun(false);
+        closeElement.current!.focus();
+      }
+      if (e.key === "Tab") {
+        console.log("yes");
       }
     }
-    window.addEventListener("click", closeSearch);
+    if (value) {
+      window.addEventListener("keydown", closeSearch);
+    }
+
     return () => {
-      return window.removeEventListener("click", closeSearch);
+      console.log("iam only run");
+      window.removeEventListener("keydown", closeSearch);
     };
-  }, [TargetElement, fun, funvalue]);
+  }, [value, fun, closeElement]);
 }
 
 export default CloseFunctoion;
