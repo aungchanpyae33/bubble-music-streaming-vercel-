81c919ba3c75a8d44825d2c3d766db1c7195f1a5
aungchanpyae3 <pyae62436@gmail.com>
2024-11-19 19:10:49 +0630

    remove edge cdn in route

--- next.config.mjs
+++ next.config.mjs
@@ -11,20 +11,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/search",
-        headers: [
-          {
-            key: "CDN-Cache-Control",
-            value:
-              "public, max-age=36000,s-maxage=36000 stale-while-revalidate=36000",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
