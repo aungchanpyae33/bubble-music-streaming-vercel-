95e3c0dca7c09f83c22b013191ab379b058d8fc8
aungchanpyae3 <pyae62436@gmail.com>
2024-11-19 19:33:06 +0630

    also change logic in bodyScrolllock , instead of check all the time scroll end  with state[], as it make re-render navSideList , to reduce this,   only calculate the scroll value when open

--- src/lib/CustomHooks/BodyScrollLock.ts
+++ src/lib/CustomHooks/BodyScrollLock.ts
@@ -1,36 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const useBodyScrollLock = (): [
   boolean,
   React.Dispatch<React.SetStateAction<boolean>>
 ] => {
   const [open, setopen] = useState(false);
-  const [scrolY, setScrollY] = useState(0);
+  const scrollY = useRef(0);
   useEffect(() => {
     if (open && window.document.body.scrollHeight >= window.innerHeight) {
-      console.log("isruntwice");
+      scrollY.current = window.scrollY;
       window.document.body.style.overflow = "hidden";
       // Prevent content jump
-      window.document.body.style.top = `-${scrolY}px`;
-
-      console.log(scrolY);
+      window.document.body.style.top = `-${scrollY.current}px`;
       window.document.body.classList.add("scrolllock");
     } else {
-      // console.log("it is ok");
-      // console.log(scrolY);
       window.document.body.classList.remove("scrolllock");
       window.document.body.style.overflow = "";
-      window.scrollTo(0, scrolY); //reslove back to top after close menubar
+
+      window.scrollTo(0, scrollY.current); //reslove back to top after close menubar
       window.document.body.style.top = "";
     }
-    const handleScrollLock = () => {
-      !open && setScrollY(window.scrollY);
-    };
-
-    window.addEventListener("scrollend", handleScrollLock); // Update scroll position on scroll
-
-    return () => window.removeEventListener("scrollend", handleScrollLock);
-  }, [open, scrolY]);
+  }, [open]);
 
   return [open, setopen];
 };
