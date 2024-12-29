9d43231d84ce0002f74fd5ed7d06ad5f95772359
aungchanpyae33 <khantlu24@gmail.com>
2024-12-29 21:38:22 +0630

    enhance ToolTip component with dynamic positioning and improved styling

--- src/ui/general/ToolTip.tsx
+++ src/ui/general/ToolTip.tsx
@@ -1,13 +1,33 @@
 "use client";
-import { ReactNode, useRef, useState } from "react";
+import clsx from "clsx";
+import { ReactNode, useEffect, useRef, useState } from "react";
 
 function ToolTip({ children }: { children: ReactNode }) {
   const [show, setShow] = useState(false);
   const setTimeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const toolTipRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const copyRef = toolTipRef.current;
+    if (show && toolTipRef.current) {
+      const tooltipRect = toolTipRef.current.getBoundingClientRect();
+      if (tooltipRect.right > window.document.body.offsetWidth) {
+        const data = tooltipRect.right - window.document.body.offsetWidth;
+        console.log(tooltipRect.right, window.document.body.offsetWidth, data);
+        toolTipRef.current.style.transform = `translateX(calc(-50% - ${data}px))`;
+      }
+    }
+
+    return () => {
+      if (copyRef) {
+        copyRef.style.transform = "";
+      }
+    };
+  }, [show]);
 
   return (
     <div
-      className="relative group w-fit max-w-full"
+      className="group relative w-fit max-w-full bg-red-950"
       onMouseEnter={() => {
         setTimeoutRef.current = setTimeout(() => {
           setShow(true);
@@ -24,11 +44,18 @@ function ToolTip({ children }: { children: ReactNode }) {
       }}
     >
       {children}
-      {show && (
-        <div className="absolute whitespace-nowrap top-0 -translate-y-[110%] left-1/2 -translate-x-1/2 outline-2 outline-blue-300 shadow-md bg-blue-50 py-1 px-4">
-          hello nice to meet you
-        </div>
-      )}
+
+      <div
+        className={clsx(
+          "absolute max-w-[500px] md:max-w-[650px] w-max  z-50 left-[50%]  top-0 -translate-x-[50%] -translate-y-[105%]  outline-2 outline-blue-300  text-sm bg-blue-500",
+          { hidden: !show }
+        )}
+        ref={toolTipRef}
+      >
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, eos
+        nemo exercitationem earum voluptates porro dignissimos numquam explicabo
+        libero ipsam!
+      </div>
     </div>
   );
 }
