1174c268e9a7a5d6f03696c3899432717e50ba50
aungchanpyae33 <khantlu24@gmail.com>
2024-12-30 13:21:55 +0630

    tooltip trigger to  children wrapper not parent to avoid inconsistent  onmouseLeave

--- src/ui/general/ToolTip.tsx
+++ src/ui/general/ToolTip.tsx
@@ -26,30 +26,31 @@ function ToolTip({ children }: { children: ReactNode }) {
   }, [show]);
 
   return (
-    <div
-      className="group relative w-fit max-w-full bg-red-950 cursor-pointer"
-      onMouseEnter={() => {
-        console.log("mouse enter");
-        setTimeoutRef.current = setTimeout(() => {
-          setShow(true);
-        }, 500);
-      }}
-      onMouseLeave={() => {
-        console.log("mouse leave");
-        if (setTimeoutRef.current) {
-          clearTimeout(setTimeoutRef.current);
-          setTimeoutRef.current = null;
-        }
-        if (show) {
-          setShow(false);
-        }
-      }}
-    >
-      {children}
+    <div className="group relative w-fit max-w-full bg-red-950 cursor-pointer">
+      <div
+        onMouseEnter={() => {
+          console.log("mouse enter");
+          setTimeoutRef.current = setTimeout(() => {
+            setShow(true);
+          }, 500);
+        }}
+        onMouseLeave={() => {
+          console.log("mouse leave");
+          if (setTimeoutRef.current) {
+            clearTimeout(setTimeoutRef.current);
+            setTimeoutRef.current = null;
+          }
+          if (show) {
+            setShow(false);
+          }
+        }}
+      >
+        {children}
+      </div>
 
       <div
         className={clsx(
-          "absolute max-w-[400px] md:max-w-[500px] w-max  z-50 left-[50%] top-0 -translate-y-[120%]  -translate-x-[50%]   outline-2 outline-blue-300  text-sm bg-blue-500",
+          "absolute max-w-[400px] md:max-w-[500px] w-max  z-50 left-[50%] top-0 -translate-y-[110%]  -translate-x-[50%]   outline-2 outline-blue-300  text-sm bg-blue-500",
           { hidden: !show }
         )}
         ref={toolTipRef}
