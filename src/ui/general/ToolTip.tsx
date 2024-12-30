e66f1d64109aed9c410022c8148cf02f39d82c0e
aungchanpyae33 <khantlu24@gmail.com>
2024-12-30 13:03:34 +0630

    change to transfrom style to left , changing transform style make Y chnage too.

--- src/ui/general/ToolTip.tsx
+++ src/ui/general/ToolTip.tsx
@@ -9,31 +9,33 @@ function ToolTip({ children }: { children: ReactNode }) {
 
   useEffect(() => {
     const copyRef = toolTipRef.current;
-    if (show && toolTipRef.current) {
-      const tooltipRect = toolTipRef.current.getBoundingClientRect();
+    if (show && copyRef) {
+      const tooltipRect = copyRef.getBoundingClientRect();
       if (tooltipRect.right > window.document.body.offsetWidth) {
         const data = tooltipRect.right - window.document.body.offsetWidth;
         console.log(tooltipRect.right, window.document.body.offsetWidth, data);
-        toolTipRef.current.style.transform = `translateX(calc(-50% - ${data}px))`;
+        copyRef.style.left = `calc(50% - ${data}px)`;
       }
     }
 
     return () => {
       if (copyRef) {
-        copyRef.style.transform = "";
+        copyRef.style.left = "";
       }
     };
   }, [show]);
 
   return (
     <div
-      className="group relative w-fit max-w-full bg-red-950"
+      className="group relative w-fit max-w-full bg-red-950 cursor-pointer"
       onMouseEnter={() => {
+        console.log("mouse enter");
         setTimeoutRef.current = setTimeout(() => {
           setShow(true);
         }, 500);
       }}
       onMouseLeave={() => {
+        console.log("mouse leave");
         if (setTimeoutRef.current) {
           clearTimeout(setTimeoutRef.current);
           setTimeoutRef.current = null;
@@ -47,14 +49,15 @@ function ToolTip({ children }: { children: ReactNode }) {
 
       <div
         className={clsx(
-          "absolute max-w-[400px] md:max-w-[500px] w-max  z-50 left-[50%]  top-0 -translate-x-[50%] -translate-y-[105%]  outline-2 outline-blue-300  text-sm bg-blue-500",
+          "absolute max-w-[400px] md:max-w-[500px] w-max  z-50 left-[50%] top-0 -translate-y-[120%]  -translate-x-[50%]   outline-2 outline-blue-300  text-sm bg-blue-500",
           { hidden: !show }
         )}
         ref={toolTipRef}
       >
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, eos
-        nemo exercitationem earum voluptates porro dignissimos numquam explicabo
-        libero ipsam!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+        blanditiis, amet voluptate quibusdam incidunt est illum vel ipsam
+        nesciunt porro!
+        {show && "hi"}
       </div>
     </div>
   );
