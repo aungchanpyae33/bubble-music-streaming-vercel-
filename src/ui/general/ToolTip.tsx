d6636fbd44e4b809137f6b05ffe106cdbc5d9396
aungchanpyae33 <khantlu24@gmail.com>
2024-12-31 18:53:06 +0630

    making tooltip postion also responsive in left screen and use one state with both value

--- src/ui/general/ToolTip.tsx
+++ src/ui/general/ToolTip.tsx
@@ -3,27 +3,42 @@ import clsx from "clsx";
 import { ReactNode, useEffect, useLayoutEffect, useRef, useState } from "react";
 
 function ToolTip({ children }: { children: ReactNode }) {
-  const [show, setShow] = useState(false);
-  const [tooltipStyle, setTooltipStyle] = useState({ left: "50%" });
+  console.log("tooltip ref");
+  const [tooltipShow, setTooltipShow] = useState({
+    show: false,
+    toolTipLeft: { left: "50%" },
+  });
   const setTimeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
   const toolTipRef = useRef<HTMLDivElement | null>(null);
 
   useLayoutEffect(() => {
     const copyRef = toolTipRef.current;
-    if (show && copyRef) {
+    if (tooltipShow.show && copyRef) {
       const tooltipRect = copyRef.getBoundingClientRect();
-      if (tooltipRect.right > window.document.body.offsetWidth) {
-        const data = tooltipRect.right - window.document.body.offsetWidth;
-        setTooltipStyle({ left: `calc(50% - ${data}px)` });
-      } else {
-        setTooltipStyle({ left: "50%" });
+      const rightEdge = tooltipRect.right;
+      const leftEdge = tooltipRect.left;
+      if (rightEdge > window.document.body.offsetWidth) {
+        const data = rightEdge - window.document.body.offsetWidth;
+        setTooltipShow((pre) => ({
+          ...pre,
+          toolTipLeft: { left: `calc(50% - ${data}px)` },
+        }));
+      } else if (leftEdge < 0) {
+        const data = Math.abs(leftEdge);
+        setTooltipShow((pre) => ({
+          ...pre,
+          toolTipLeft: { left: `calc(50% + ${data}px)` },
+        }));
       }
     }
 
     return () => {
-      setTooltipStyle({ left: "50%" });
+      setTooltipShow((pre) => ({
+        ...pre,
+        toolTipLeft: { left: "50%" },
+      }));
     };
-  }, [show]);
+  }, [tooltipShow.show]);
 
   return (
     <div className="group relative w-fit max-w-full bg-red-950 cursor-pointer">
@@ -31,7 +46,10 @@ function ToolTip({ children }: { children: ReactNode }) {
         onMouseEnter={() => {
           console.log("mouse enter");
           setTimeoutRef.current = setTimeout(() => {
-            setShow(true);
+            setTooltipShow((pre) => ({
+              ...pre,
+              show: true,
+            }));
           }, 500);
         }}
         onMouseLeave={() => {
@@ -40,8 +58,11 @@ function ToolTip({ children }: { children: ReactNode }) {
             clearTimeout(setTimeoutRef.current);
             setTimeoutRef.current = null;
           }
-          if (show) {
-            setShow(false);
+          if (tooltipShow.show) {
+            setTooltipShow((pre) => ({
+              ...pre,
+              show: false,
+            }));
           }
         }}
       >
@@ -51,15 +72,15 @@ function ToolTip({ children }: { children: ReactNode }) {
       <div
         className={clsx(
           "absolute max-w-[400px] md:max-w-[500px] w-max  z-50 top-0 -translate-y-[110%] left-[50%]  -translate-x-[50%]   outline-2 outline-blue-300  text-sm bg-blue-500",
-          { hidden: !show }
+          { hidden: !tooltipShow.show }
         )}
-        style={tooltipStyle}
+        style={tooltipShow.toolTipLeft}
         ref={toolTipRef}
       >
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
         blanditiis, amet voluptate quibusdam incidunt est illum vel ipsam
         nesciunt porro!
-        {show && "hi"}
+        {tooltipShow.show && "hi"}
       </div>
     </div>
   );
