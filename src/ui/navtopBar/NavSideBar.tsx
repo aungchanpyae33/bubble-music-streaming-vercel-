d028d9b1219bac9a7d203c3494cf4abbc6235353
aungchanpyae33 <khantlu24@gmail.com>
2025-01-11 20:35:57 +0630

    fix(NavSideBar): adjust height for consistent button sizing in navigation

--- src/ui/navtopBar/NavSideBar.tsx
+++ src/ui/navtopBar/NavSideBar.tsx
@@ -8,12 +8,12 @@ function NavSideBar() {
     <div>
       <NavList
         childrenExplore={
-          <div className=" w-[70px] max-w-[70px]  min-h-[50px]  flex items-center justify-center bg-black text-white   ">
+          <div className=" w-[70px] max-w-[70px] h-[50px]  flex items-center justify-center bg-black text-white   ">
             <div className="truncate">Explore</div>
           </div>
         }
         childrenLive={
-          <div className=" w-[70px] min-h-[50px]  flex items-center justify-center bg-black text-white">
+          <div className=" w-[70px] h-[50px]  flex items-center justify-center bg-black text-white">
             Live
           </div>
         }
