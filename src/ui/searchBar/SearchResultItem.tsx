403d877b93aeb1cf5d73b6f4659bd6e02acc78c2
aungchanpyae33 <khantlu24@gmail.com>
2025-01-24 20:18:54 +0630

    cerate seperate searchResultItem component

---
+++ src/ui/searchBar/SearchResultItem.tsx
@@ -0,0 +1,35 @@
+import clsx from "clsx";
+import Link from "next/link";
+import React, { memo } from "react";
+interface prop {
+  title: string;
+  index: number;
+  // arrow: {
+  //   run: boolean;
+  //   number: number;
+  // };
+  show: boolean;
+}
+const SearchResultItem = memo(({ title, index, show }: prop) => {
+  console.log("SearchResultItem");
+  return (
+    <div
+      className={clsx(
+        "hover:bg-red-600 pl-20 sm:pl-2 h-[40px] flex items-center",
+        {
+          "bg-red-600": show,
+        }
+      )}
+      style={{ cursor: "pointer" }}
+      key={title}
+    >
+      <Link href={`/test?query=${title}`} tabIndex={-1}>
+        {title}
+      </Link>
+    </div>
+  );
+});
+
+SearchResultItem.displayName = "SearchResultItem";
+
+export default SearchResultItem;
