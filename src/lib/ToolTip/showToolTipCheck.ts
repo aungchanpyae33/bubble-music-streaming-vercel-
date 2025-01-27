70fb68b56d22cb4a689dbfe0c9f81514af29eb34
aungchanpyae33 <khantlu24@gmail.com>
2025-01-27 15:34:34 +0630

    my mistake when migrating to seperate file , use different check for onEnter and onWheel

--- src/lib/ToolTip/showToolTipCheck.ts
+++ src/lib/ToolTip/showToolTipCheck.ts
@@ -1,14 +1,14 @@
 import React, { RefObject } from "react";
 import type { tooltipState } from "../CustomHooks/TooltipOverflow";
-export function isInside(
+export function isInsideForEnter(
   targetElement: HTMLDivElement,
   e: React.WheelEvent<HTMLDivElement> | React.MouseEvent<HTMLDivElement>
 ) {
   const { clientX: x, clientY: y } = e;
   const rect = targetElement.getBoundingClientRect();
-  const isPointerInside =
-    x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom;
-  return isPointerInside;
+  const isPointerInsideForEnter =
+    x >= rect.left || x <= rect.right || y >= rect.top || y <= rect.bottom;
+  return isPointerInsideForEnter;
 }
 export const showToolTipCheck = (
   setTimeoutRef: RefObject<ReturnType<typeof setTimeout> | null>,
@@ -16,7 +16,31 @@ export const showToolTipCheck = (
   setTooltipShow: React.Dispatch<React.SetStateAction<tooltipState>>,
   targetElement: HTMLDivElement,
   e: React.WheelEvent<HTMLDivElement> | React.MouseEvent<HTMLDivElement>
-) => {
+) {
+  const { clientX: x, clientY: y } = e;
+  const rect = targetElement.getBoundingClientRect();
+  const isPointerInsideForEnter =
+    x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom;
+  return isPointerInsideForEnter;
+}
+
+interface TooltipProps {
+  setTimeoutRef: RefObject<ReturnType<typeof setTimeout> | null>;
+  tooltipShow: tooltipState;
+  setTooltipShow: React.Dispatch<React.SetStateAction<tooltipState>>;
+  targetElement: HTMLDivElement;
+  e: React.WheelEvent<HTMLDivElement> | React.MouseEvent<HTMLDivElement>;
+  isEnterEvent: boolean;
+}
+
+export const showToolTipCheck = ({
+  setTimeoutRef,
+  tooltipShow,
+  setTooltipShow,
+  targetElement,
+  e,
+  isEnterEvent,
+}: TooltipProps) => {
   setTimeoutRef.current = setTimeout(() => {
     const isPointerInside = isInside(targetElement, e);
     if (isPointerInside && !tooltipShow.show) {
