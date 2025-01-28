123b926afecd0e2b7aa025b6824edd4174f10e12
aungchanpyae33 <khantlu24@gmail.com>
2025-01-28 23:27:53 +0630

    feat: add GET endpoint for search functionality in API

---
+++ src/app/api/search/route.ts
@@ -0,0 +1,19 @@
+export const runtime = "edge";
+
+import { getData } from "@/database/data";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query: string | null = searchParams.get("with");
+  if (query) {
+    console.log(query);
+    const data = await getData(query);
+
+    return NextResponse.json(data, {
+      headers: {
+        "CDN-Cahche-Control": "public,max-age=0,smax-age=360000",
+      },
+    });
+  }
+}
