7f78d4fbddd7b9eb162ff403aa75ff98a1593e93
aungchanpyae33 <khantlu24@gmail.com>
2025-01-29 16:37:50 +0630

    no need to use animate state and sm: prefix , they are now sharing the same ui

--- src/ui/navtopBar/NavListUlWrapper.tsx
+++ src/ui/navtopBar/NavListUlWrapper.tsx
@@ -29,18 +29,10 @@ function NavListUlWrapper({
         className={clsx(
           "fixed top-0 z-30 box-border  left-0 h-[100%] flex  flex-col gap-1  bg-green-500  rounded-b-sm",
           {
-            "-translate-x-full opacity-0 sm:-translate-x-0 sm:opacity-100 ":
-              !open,
+            "-translate-x-full opacity-0  ": !open,
             "translate-x-0 opacity-100": open,
-            "duration-200 transition-[transform,opacity]": animate,
           }
         )}
-        onTransitionEnd={() => {
-          console.log("hello");
-          if (animate) {
-            setAnimate(false);
-          }
-        }}
       >
         <li className="h-[50px] z-10 relative will-change-transform">
           <button
@@ -55,15 +47,9 @@ function NavListUlWrapper({
           </button>
           <button
             className={clsx(
-              "absolute -z-10  top-0 px-2   flex  w-[150px] min-w-[150px] max-w-[150px] items-center   justify-start h-[50px] sm:duration-200 sm:transition-[transform,opacity]   bg-green-500",
-              {
-                "-translate-x-[calc(100%-70px)] opacity-0 ": open === false,
-                "translate-x-[70px] opacity-100": open === true,
-              }
+              "px-2   flex  w-[150px] min-w-[150px] max-w-[150px] items-center   justify-start h-[50px]"
             )}
-            onTransitionEnd={(e) => {
-              e.stopPropagation();
-            }}
+            aria-hidden={true}
             tabIndex={open ? 0 : -1}
             aria-hidden={true}
           >
