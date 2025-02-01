0dcdaf418b7e0bb160fda50345ff923cdbf4c3ef
aungchanpyae33 <khantlu24@gmail.com>
2025-02-01 23:51:28 +0630

    instead of adding one by one add all at one

--- src/lib/zustand.ts
+++ src/lib/zustand.ts
@@ -48,7 +48,7 @@ export const useCurrentPlayList = create<
   ],
   setPlayListArray: (newList: SongDetail[]) =>
     set(() => ({
-      playListArray: [...newList],
+      playListArray: newList,
     })),
 }));
 
