1d43f076ddeb6f363a5f8379296a12cac790428d
aungchanpyae33 <khantlu24@gmail.com>
2025-02-09 20:06:13 +0630

    fix: round seconds and minutes in TimeFormat function for improved accuracy

--- src/lib/TimeFormat.ts
+++ src/lib/TimeFormat.ts
@@ -1,7 +1,7 @@
 export const TimeFormat = (sec: number | undefined) => {
   if (sec) {
-    const minutes = Math.floor(sec / 60);
-    const seconds = Math.floor(sec % 60);
+    const minutes = Math.round(sec / 60);
+    const seconds = Math.round(sec % 60);
     const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
     return `${minutes}:${returnedSeconds}`;
   } else {
