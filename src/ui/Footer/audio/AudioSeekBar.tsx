91b4aff4c8f01907e5d43826af577fd1033f4db1
aungchanpyae33 <khantlu24@gmail.com>
2025-02-10 10:14:53 +0630

    instead of updating dom ,although it is safe in the eventhandler , but use state update for updating time

--- src/ui/Footer/audio/AudioSeekBar.tsx
+++ src/ui/Footer/audio/AudioSeekBar.tsx
@@ -8,6 +8,7 @@ import {
 import throttle from "@/lib/throttle";
 import { TimeFormat } from "@/lib/TimeFormat";
 import { RefObject, useContext, useEffect, useRef, useState } from "react";
+import TimeIndicatorCur from "./TimeIndicatorCur";
 export interface eventProp {
   e:
     | React.MouseEvent<HTMLInputElement>
@@ -34,7 +35,7 @@ function AudioSeekBar({ duration, dataCur }: PropAudioSeek) {
   const [value, setValue] = useState<number>(100);
   const sliderRef = useRef<HTMLDivElement | null>(null);
   const progressRef = useRef<HTMLDivElement | null>(null);
-
+  const [timePosition, setTimePosition] = useState(0);
   const [isDragging, setIsDragging] = useState(false);
   useEffect(() => {
     const copyDataAudio = dataAudio!.current!;
@@ -44,7 +45,7 @@ function AudioSeekBar({ duration, dataCur }: PropAudioSeek) {
       const data = 100 - newValue;
       const currentTime = (data / 100) * duration;
       setValue(newValue);
-      dataCur!.current!.textContent = TimeFormat(currentTime);
+      setTimePosition(currentTime);
     }
     function handleMouseUp(e: MouseEvent) {
       setIsDragging(false);
@@ -76,7 +77,7 @@ function AudioSeekBar({ duration, dataCur }: PropAudioSeek) {
         const newValue = 100 - data;
         // console.log(newValue);
         setValue(newValue);
-        dataCur!.current!.textContent = TimeFormat(currentTime);
+        setTimePosition(currentTime);
       }
     }
     if (isDragging) {
@@ -112,77 +113,81 @@ function AudioSeekBar({ duration, dataCur }: PropAudioSeek) {
     duration
   );
   return (
-    <div
-      className="ml-10 border-2 group  h-[30px] w-[600px] flex items-center"
-      tabIndex={0}
-      onKeyDown={(e) => {
-        if (e.key === "ArrowRight") {
-          if (!sliderRef.current) return;
-          setIsDragging(true);
-          const newValue = Math.max(value - 1, 0);
-          setValue(newValue);
-        } else if (e.key === "ArrowLeft") {
-          if (!sliderRef.current) return;
-          setIsDragging(true);
-          const newValue = Math.min(value + 1, 100);
-          setValue(newValue);
-        } else if (e.key !== "Tab") {
-          e.preventDefault();
-        }
-      }}
-      onKeyUp={(e) => {
-        if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
-          setIsDragging(false);
-          const offsetWidth =
-            progressRef!.current!.getBoundingClientRect().width;
-          const per = Math.min(
-            Math.max(
-              offsetWidth / sliderRef!.current!.getBoundingClientRect().width,
-              0
-            ),
-            1
-          );
-          const data = per * duration;
-          segNum.current = playBackRate({ dataAudio, data, sege, duration });
-          loadNextSegment();
-        } else if (e.key !== "Tab") {
-          e.preventDefault();
-        }
-      }}
-    >
+    <>
+      <TimeIndicatorCur timePosition={timePosition} />
+
       <div
-        className="flex-1 h-[8px]  flex bg-blue-700 relative select-none"
-        ref={sliderRef}
-        onMouseDown={(e) => {
-          if (!sliderRef.current) return;
-          setIsDragging(true);
-          const newValue = sliderPositionCal({ sliderRef, e });
-          const data = 100 - newValue;
-          const currentTime = (data / 100) * duration;
-          setValue(newValue);
-          dataCur!.current!.textContent = TimeFormat(currentTime);
+        className="ml-10 border-2 group  h-[30px] w-[600px] flex items-center"
+        tabIndex={0}
+        onKeyDown={(e) => {
+          if (e.key === "ArrowRight") {
+            if (!sliderRef.current) return;
+            setIsDragging(true);
+            const newValue = Math.max(value - 1, 0);
+            setValue(newValue);
+          } else if (e.key === "ArrowLeft") {
+            if (!sliderRef.current) return;
+            setIsDragging(true);
+            const newValue = Math.min(value + 1, 100);
+            setValue(newValue);
+          } else if (e.key !== "Tab") {
+            e.preventDefault();
+          }
         }}
-        onTouchStart={(e) => {
-          if (!sliderRef.current) return;
-          setIsDragging(true);
-          const newValue = sliderPositionCal({ sliderRef, e });
-          setValue(newValue);
+        onKeyUp={(e) => {
+          if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
+            setIsDragging(false);
+            const offsetWidth =
+              progressRef!.current!.getBoundingClientRect().width;
+            const per = Math.min(
+              Math.max(
+                offsetWidth / sliderRef!.current!.getBoundingClientRect().width,
+                0
+              ),
+              1
+            );
+            const data = per * duration;
+            segNum.current = playBackRate({ dataAudio, data, sege, duration });
+            loadNextSegment();
+          } else if (e.key !== "Tab") {
+            e.preventDefault();
+          }
         }}
       >
         <div
-          className="bg-red-400  absolute top-0 left-0 h-full"
-          style={{
-            right: `${value}%`,
+          className="flex-1 h-[8px]  flex bg-blue-700 relative select-none"
+          ref={sliderRef}
+          onMouseDown={(e) => {
+            if (!sliderRef.current) return;
+            setIsDragging(true);
+            const newValue = sliderPositionCal({ sliderRef, e });
+            const data = 100 - newValue;
+            const currentTime = (data / 100) * duration;
+            setValue(newValue);
+            setTimePosition(currentTime);
           }}
-          ref={progressRef}
-        ></div>
+          onTouchStart={(e) => {
+            if (!sliderRef.current) return;
+            setIsDragging(true);
+            const newValue = sliderPositionCal({ sliderRef, e });
+            setValue(newValue);
+          }}
+        >
+          <div
+            className="bg-red-400  absolute top-0 left-0 h-full"
+            style={{
+              right: `${value}%`,
+            }}
+            ref={progressRef}
+          ></div>
 
-        <span
-          className="absolute group-hover:inline hidden group-focus:inline    w-[20px] rounded-full h-[20px] top-1/2 -translate-y-1/2 bg-black -translate-x-[10px]"
-          style={{ left: `calc(100% - ${value}%)` }}
-        ></span>
+          <span
+            className="absolute group-hover:inline hidden group-focus:inline    w-[20px] rounded-full h-[20px] top-1/2 -translate-y-1/2 bg-black -translate-x-[10px]"
+            style={{ left: `calc(100% - ${value}%)` }}
+          ></span>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
