51bf864c4b7e29ebd2d9143024b98a3ac85069fa
aungchanpyae33 <khantlu24@gmail.com>
2025-02-10 10:16:28 +0630

    change logic timeIndicatorCur component and now it can accpect timevalue

--- src/ui/Footer/audio/TimeIndicatorCur.tsx
+++ src/ui/Footer/audio/TimeIndicatorCur.tsx
@@ -1,16 +1,6 @@
 import { TimeFormat } from "@/lib/TimeFormat";
-import { RefObject } from "react";
-function TimeIndicatorCur({
-  dataCur,
-}: {
-  dataCur: RefObject<HTMLSpanElement | null>;
-}) {
-  console.log("render timeindicator");
-  return (
-    <span ref={dataCur} className="time">
-      {TimeFormat(0)}
-    </span>
-  );
+function TimeIndicatorCur({ timePosition }: { timePosition: number }) {
+  return <span className="time ">{TimeFormat(timePosition)}</span>;
 }
 
 export default TimeIndicatorCur;
