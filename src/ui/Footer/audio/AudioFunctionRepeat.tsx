4df54e079eabb77b9e804ab1b23ae65877d9ec56
aungchanpyae33 <khantlu24@gmail.com>
2025-02-12 23:47:59 +0630

    implement repeat toggle button using zustand state management

--- src/ui/Footer/audio/AudioFunctionRepeat.tsx
+++ src/ui/Footer/audio/AudioFunctionRepeat.tsx
@@ -1,5 +1,16 @@
+import { IsRepeatState, RepeatAction, useRepeat } from "@/lib/zustand";
+
 function AudioFunctionRepeat() {
-  return <button className="bg-pink-300 p-2 text-sm md:text-base">Repe</button>;
+  const isRepeat = useRepeat((state: IsRepeatState) => state.isRepeat);
+  const setRepeat = useRepeat((state: RepeatAction) => state.setRepeat);
+  return (
+    <button
+      className="bg-pink-300 p-2 text-sm md:text-base"
+      onClick={() => setRepeat()}
+    >
+      {isRepeat ? "unre" : "repe"}
+    </button>
+  );
 }
 
 export default AudioFunctionRepeat;
