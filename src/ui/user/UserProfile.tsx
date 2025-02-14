14137f88a9c9b9405006ed5d1c5376c6e9d635a8
aungchanpyae33 <khantlu24@gmail.com>
2025-02-14 22:29:01 +0630

    feat: integrate OutterClick hook to manage clicks outside the UserProfile component

--- src/ui/user/UserProfile.tsx
+++ src/ui/user/UserProfile.tsx
@@ -2,14 +2,17 @@
 import { useRef, useState } from "react";
 import { clsx } from "clsx";
 import CloseFunctoion from "@/lib/CloseFunction";
+import OutterClick from "@/lib/OutterClick";
+
 function UserProfile({ name }: { name: string }) {
   const userName = name.slice(0, 1);
   const [userOpen, setUserOpen] = useState(false);
   const buttonRef = useRef<HTMLButtonElement | null>(null);
+  const parentRef = useRef<HTMLDivElement | null>(null);
   CloseFunctoion(userOpen, setUserOpen, buttonRef);
-
+  OutterClick(userOpen, setUserOpen, parentRef);
   return (
-    <div className="">
+    <div className=" relative z-10" ref={parentRef}>
       <button
         ref={buttonRef}
         className="bg-green-500 w-[40px] h-[40px] rounded-full flex items-center justify-center"
