86ebadf6630e402f2435ce6bd25555dd29f65ad2
aungchanpyae33 <khantlu24@gmail.com>
2025-02-16 23:32:57 +0630

    instead of rely on timeposition , rely on value and calculate time

--- src/ui/Footer/audio/TimeIndicatorCur.tsx
+++ src/ui/Footer/audio/TimeIndicatorCur.tsx
@@ -1,6 +1,14 @@
 import { TimeFormat } from "@/lib/TimeFormat";
-function TimeIndicatorCur({ timePosition }: { timePosition: number }) {
-  return <span className="time ">{TimeFormat(timePosition)}</span>;
+function TimeIndicatorCur({
+  value,
+  duration,
+}: {
+  value: number;
+  duration: number;
+}) {
+  const data = 100 - value;
+  const currentTime = (data / 100) * duration;
+  return <span className="time ">{TimeFormat(currentTime)}</span>;
 }
 
 export default TimeIndicatorCur;
