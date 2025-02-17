8383c035916e570caaa23c5016fcb4aca67696a1
aungchanpyae33 <khantlu24@gmail.com>
2025-02-17 16:45:14 +0630

    refactor: simplify AudioElement by removing unused state and refs in AudioSeekBar

--- src/ui/Footer/audio/AudioElement.tsx
+++ src/ui/Footer/audio/AudioElement.tsx
@@ -1,29 +1,18 @@
-import { TimeFormat } from "@/lib/TimeFormat";
-import { ReactNode, useContext, useRef, useState } from "react";
+import { ReactNode, useContext } from "react";
 import DataContext from "@/lib/MediaSource/ContextMedia";
-import AudioSeekBar from "./AudioSeekBar";
+import AudioSeekBar from "./SliderUi/AudioSeekBar";
 import AudioWrapper from "./AudioWrapper";
 export interface PropTime {
   cur: number;
   durationTime: number | undefined;
 }
 function AudioElement({ Child, url }: { Child: ReactNode; url: string }) {
-  const dataInput = useRef<HTMLInputElement>(null);
-  const [bottom, setBottom] = useState(true);
-  const dataCur = useRef<HTMLSpanElement>(null);
   const { dataAudio, duration } = useContext(DataContext);
 
   return (
     <div className=" w-full flex items-center ">
       <AudioWrapper dataAudio={dataAudio} url={url} />
-      <AudioSeekBar
-        dataCur={dataCur}
-        bottom={bottom}
-        setBottom={setBottom}
-        duration={duration}
-        dataInput={dataInput}
-        key={url}
-      />
+      <AudioSeekBar duration={duration} key={url} />
       {Child}
     </div>
   );
