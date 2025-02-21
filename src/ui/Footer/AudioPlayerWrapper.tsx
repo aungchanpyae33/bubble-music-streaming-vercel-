12c0667144c7033f82ddeacb2442283ea19e2ef7
aungchanpyae33 <khantlu24@gmail.com>
2025-02-21 23:11:04 +0630

    move contextMediaAudioFull to audioplayerWrapper

--- src/ui/Footer/AudioPlayerWrapper.tsx
+++ src/ui/Footer/AudioPlayerWrapper.tsx
@@ -1,11 +1,18 @@
 "use client";
+import ContextMediaAudioFull from "@/lib/MediaSource/ContextMediaAudioFull";
 import MediaSessionDesWrapper from "./audio/MediaSessionWrapper/MediaSessionDesWrapper";
 import AudioPlayer from "./AudioPLayer";
 
-function AudioPlayerWrapper() {
+function AudioPlayerWrapper({
+  footerRef,
+}: {
+  footerRef: React.RefObject<HTMLElement | null>;
+}) {
   return (
     <MediaSessionDesWrapper>
-      <AudioPlayer></AudioPlayer>
+      <ContextMediaAudioFull footerRef={footerRef}>
+        <AudioPlayer footerRef={footerRef} />
+      </ContextMediaAudioFull>
     </MediaSessionDesWrapper>
   );
 }
--- src/ui/Footer/FooterBar.tsx
+++ src/ui/Footer/FooterBar.tsx
@@ -1,9 +1,15 @@
+"use client";
+import { useRef } from "react";
 import AudioPlayerWrapper from "./AudioPlayerWrapper";
 function FooterBar() {
+  const footerRef = useRef<HTMLElement | null>(null);
   return (
-    <div className=" w-full h-full bg-red-900">
-      <AudioPlayerWrapper />
-    </div>
+    <footer
+      className="fixed bottom-0 w-full  flex bg-white  items-center  h-[70px]"
+      ref={footerRef}
+    >
+      <AudioPlayerWrapper footerRef={footerRef} />
+    </footer>
   );
 }
 
