f8b448797b5ef1322972da9bbffffe23977f95ee
aungchanpyae33 <khantlu24@gmail.com>
2025-02-21 23:09:37 +0630

    feat: add FullToggleButton component for toggling audio display

---
+++ src/ui/Footer/audioFull/FullToggleButton.tsx
@@ -0,0 +1,25 @@
+import { Context } from "@/lib/MediaSource/ContextMediaAudioFull";
+import { useContext } from "react";
+function FullToggleButton({
+  footerRef,
+}: {
+  footerRef: React.RefObject<HTMLElement | null>;
+}) {
+  const { open, setopenFn } = useContext(Context);
+  console.log(open);
+  return (
+    <>
+      <button
+        className="bg-black text-white p-1 hidden md:inline-block"
+        onClick={() => {
+          footerRef!.current!.classList.toggle("z-30");
+          setopenFn();
+        }}
+      >
+        open
+      </button>
+    </>
+  );
+}
+
+export default FullToggleButton;
