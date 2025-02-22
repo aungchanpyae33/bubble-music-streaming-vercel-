30602c892eac70ebc9cd696bddff683fed92f885
aungchanpyae33 <khantlu24@gmail.com>
2025-02-22 23:44:13 +0630

    style: increase z-index for NavBar and OverLay components to improve layering

--- src/ui/navtopBar/NavBar.tsx
+++ src/ui/navtopBar/NavBar.tsx
@@ -5,18 +5,18 @@ import NavSideBar from "./NavSideBar";
 
 function NavBar() {
   return (
-    <nav className="navBarContainer flex w-full sticky  top-0  bg-red-500 h-[50px] z-20 items-center justify-between ">
+    <nav className="navBarContainer flex w-full sticky  top-0   bg-red-500 h-[50px] z-40 items-center justify-between ">
       <div className="w-[70px]">
         <NavSideBar />
       </div>
       <div className="logo  px-2">
         <Link href="/">Bubble</Link>
       </div>
-      <div className=" flex-1 z-20   text-end">
+      <div className=" flex-1 z-20 text-end">
         <SearchBar />
       </div>
 
-      <ul className="navLinkContainer z-10  w-[20%] flex flex-wrap self-stretch  justify-around">
+      <ul className="navLinkContainer z-10   w-[20%] flex flex-wrap self-stretch  justify-around">
         <li className="flex items-center">hi</li>
         <li className="flex items-center">hi</li>
         <li className="  flex items-center relative">
