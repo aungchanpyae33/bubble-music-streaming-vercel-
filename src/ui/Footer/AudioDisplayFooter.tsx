9b5a0eba9f6e9ab6c8b3b005c14fac13c771280d
aungchanpyae33 <khantlu24@gmail.com>
2025-02-22 23:45:31 +0630

    fix: update AudioDisplayFooter to use full width and height for image rendering

--- src/ui/Footer/AudioDisplayFooter.tsx
+++ src/ui/Footer/AudioDisplayFooter.tsx
@@ -2,12 +2,11 @@ import Image from "next/image";
 
 function AudioDisplayFooter({ urlImage }: { urlImage: string }) {
   return (
-    <div className=" p-1  w-[70px] h-[70px] bg-blue-600   flex items-center justify-center shadow-md ">
-      <div className=" w-[70px]">
+    <div className=" p-1  w-[70px] h-[70px]   flex items-center justify-center shadow-md ">
+      <div className=" w-full h-full relative">
         <Image
           src={urlImage}
-          width={300}
-          height={300}
+          fill
           alt="test image"
           className="w-full h-full"
           priority={true}
