759647bb3c06dceb1b2b4a09eb1d732ad77ae0b4
aungchanpyae33 <khantlu24@gmail.com>
2025-02-25 12:02:36 +0630

    no need to use that as it will handle in parent

--- src/ui/Footer/audioFull/AudioFullBackGround.tsx
+++ src/ui/Footer/audioFull/AudioFullBackGround.tsx
@@ -35,10 +35,7 @@ function AudioFullBackGround({
     <div
       onClick={(e) => e.stopPropagation()}
       className={clsx(
-        "z-50   fixed inset-0 bg-no-repeat bg-cover bg-center bg-fixed",
-        {
-          hidden: !open,
-        }
+        "z-50   fixed inset-0 bg-no-repeat bg-cover bg-center bg-fixed"
       )}
       style={{
         backgroundImage:
