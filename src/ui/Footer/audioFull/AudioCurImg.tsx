5eb876545b7fd57d21795cc5e574d16401885d99
aungchanpyae33 <khantlu24@gmail.com>
2025-02-26 21:01:02 +0630

    feat: refactor AudioCurImg component to use AudioInfoImg and integrate dominant color extraction and sent dominant color back to parent audioFullbackground

--- src/ui/Footer/audioFull/AudioCurImg.tsx
+++ src/ui/Footer/audioFull/AudioCurImg.tsx
@@ -1,27 +1,21 @@
 import clsx from "clsx";
-import Image from "next/image";
-import { useState } from "react";
+import { useContext, useRef } from "react";
+import { Context } from "./AudioFullBackGround";
+import useGetDominantColor from "@/lib/GetDominantColor";
+import AudioInfoImg from "./AudioInfoImg";
 
 function AudioCurImg() {
-  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const imgRef = useRef<HTMLImageElement | null>(null);
+  const { setBgValue } = useContext(Context);
+  const [isImageLoaded] = useGetDominantColor({ setBgValue, imgRef });
+
   return (
     <div
       className={clsx(
-        "md:w-[200px] shrink-0 mx-auto md:mx-0   lg:w-[250px] w-[100%] sm:w-[60%] max-w-[368px]  before:block before:pt-[100%]    duration-1000 bg-green-500 rounded-md overflow-hidden relative"
+        "md:w-[200px] shrink-0 mx-auto md:mx-0   lg:w-[250px] w-[100%] sm:w-[60%] max-w-[485px]   before:block before:pt-[100%] bg-[#E0E0E0] rounded-md overflow-hidden relative"
       )}
     >
-      <Image
-        className={clsx("transition-opacity duration-1000", {
-          "opacity-0": !isImageLoaded,
-          "opacity-100": isImageLoaded,
-        })}
-        src={
-          "https://s3.tebi.io/test1345/timo-volz-ZlFKIG6dApg-unsplash%20%281%29.jpg"
-        }
-        alt="this is image element"
-        fill
-        onLoad={() => setIsImageLoaded(true)}
-      />
+      <AudioInfoImg isImageLoaded={isImageLoaded} imgRef={imgRef} />
     </div>
   );
 }
