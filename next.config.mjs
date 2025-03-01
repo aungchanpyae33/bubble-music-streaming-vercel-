301342c630310772da232add02045c35fd57654e
aungchanpyae33 <khantlu24@gmail.com>
2025-03-01 19:40:17 +0630

    add: enable experimental PPR feature in Next.js configuration and update Next.js version

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    ppr: true,
+  },
+
   // reactStrictMode: false,
   images: {
     formats: ["image/avif", "image/webp"],
