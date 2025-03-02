0b3a332b9d4b580c6ff23c0b30b00a48840ac8d1
aungchanpyae33 <khantlu24@gmail.com>
2025-03-02 19:59:26 +0630

    feat: enhance FooterBar to use DeviceCheck for user agent detection

--- src/ui/Footer/FooterBar.tsx
+++ src/ui/Footer/FooterBar.tsx
@@ -1,6 +1,14 @@
+import { DeviceCheck } from "@/lib/DeviceCheck";
 import AudioPlayerWrapper from "./AudioPlayerWrapper";
-function FooterBar() {
-  return <AudioPlayerWrapper />;
+import DeviceContextFooter from "@/lib/DeviceContext/DeviceContextFooter";
+async function FooterBar() {
+  //consider to use 'use cache' (it is very new feature)
+  const deviceFromUserAgent = await DeviceCheck();
+  return (
+    <DeviceContextFooter device={deviceFromUserAgent}>
+      <AudioPlayerWrapper />
+    </DeviceContextFooter>
+  );
 }
 
 export default FooterBar;
