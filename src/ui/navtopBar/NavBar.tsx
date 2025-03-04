d2e5ba0e5267445acdff8b39c42634ed69689bfd
aungchanpyae33 <khantlu24@gmail.com>
2025-03-04 23:56:30 +0630

    ui : no need to fix at top  and improve style

--- src/ui/navtopBar/NavBar.tsx
+++ src/ui/navtopBar/NavBar.tsx
@@ -6,7 +6,7 @@ import { Suspense } from "react";
 
 function NavBar() {
   return (
-    <nav className="navBarContainer flex w-full sticky  top-0   bg-red-500  h-[50px] z-40 items-center justify-between ">
+    <nav className="navBarContainer flex w-full    bg-red-500  h-[50px]  items-center justify-between ">
       <div className="w-[70px]">
         <Suspense fallback={<p>lorem500</p>}>
           <NavSideBar />
@@ -15,11 +15,11 @@ function NavBar() {
       <div className="logo  px-2">
         <Link href="/">Bubble</Link>
       </div>
-      <div className=" flex-1 z-20 text-end">
+      <div className=" flex-1 z-20  text-end">
         <SearchBar />
       </div>
 
-      <ul className="navLinkContainer z-10   w-[20%] flex flex-wrap self-stretch  justify-around">
+      <ul className="navLinkContainer   w-[20%] flex flex-wrap self-stretch  justify-around">
         <li className="flex items-center">hi</li>
         <li className="flex items-center">hi</li>
         <li className="  flex items-center relative">
