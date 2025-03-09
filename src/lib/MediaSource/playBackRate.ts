900252d7cea1d30e36cff3ef2a056af603c97a73
aungchanpyae33 <khantlu24@gmail.com>
2025-03-09 22:02:52 +0630

    fix: update playBackRate function to return segment data and improve buffer handling

--- src/lib/MediaSource/playBackRate.ts
+++ src/lib/MediaSource/playBackRate.ts
@@ -17,10 +17,10 @@ export function playBackRate({
   bufferThreshold,
 }: prop) {
   dataAudio.current!.currentTime = data;
-  const remainingBuffer = getRemainingBufferDuration(dataAudio);
+  const { remainingBuffer, segData } = getRemainingBufferDuration(dataAudio);
   // console.log("10", remainingBuffer);
   //only return updated the segNum with conditional to prevent segment chagne to previous already loaded segment
-  if (bufferThreshold < remainingBuffer) return undefined;
+
   const audioPosition = Math.floor((data / duration!) * 100);
   const datatest = [
     10.005, 19.989, 29.995, 40, 50.005, 59.989, 69.995, 80, 90.005, 99.989,
@@ -30,7 +30,7 @@ export function playBackRate({
   // change to math.round to get data more precies
   // it can be greate if can fetch needed data it self (further) but it can not be fully accurate cuz all segemetns are not the same size
 
-  let segPosition = Math.round((sege! * audioPosition) / 100);
+  let segPosition = Math.floor((sege! * audioPosition) / 100);
   let final = segPosition;
   for (
     let index = data;
@@ -39,5 +39,14 @@ export function playBackRate({
   ) {
     final++;
   }
-  return final;
+
+  // if already buffered. return segData , if not return dataG by conditional check to make sure that no additional load on if there was already loaded buffer segments
+
+  if (bufferThreshold < remainingBuffer) return segData;
+  // data+2 means already loaded buffer + next unloaded segement
+  //data + 1 means already loaded buffer
+  //  in this case SegeData -1 is equal to the segData + 1 which is data of the segments that has loaded buffer
+  const datag = final === segData - 1 ? final + 1 : final;
+  console.log(datag);
+  return datag;
 }
