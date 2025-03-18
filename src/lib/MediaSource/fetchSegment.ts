5a4cda52f1dbb8baa2cb12eed30694bfe189b8e3
aungchanpyae33 <khantlu24@gmail.com>
2025-03-18 14:20:42 +0630

    fix : refactor fetchSegment to use async/await for improved error handling and readability

--- src/lib/MediaSource/fetchSegment.ts
+++ src/lib/MediaSource/fetchSegment.ts
@@ -1,5 +1,6 @@
-import React, { RefObject } from "react";
-export const fetchSegment = (
+import { RefObject } from "react";
+
+export const fetchSegment = async (
   url: string,
   sourceBuffer: RefObject<SourceBuffer | null>,
   mediaSource: RefObject<MediaSource | null>,
@@ -10,36 +11,30 @@ export const fetchSegment = (
     signal: abortController!.current!.signal,
   };
 
-  // https://bubbleapideno.netlify.app
-  // fetch(`https://bubblemusicapi.vercel.app/api?with=${outputUrl}`, fetchOptions)
-
-  // `https://jolly-sun-bbad.bubblemusic990.workers.dev/api?with=${outputUrl}`,
   const outputUrl =
     segNum !== undefined ? url.replace("init.mp4", `seg-${segNum}.m4s`) : url;
-  //  `${segNum ? videoSegments[segNum] : videoSegments[0]}`
-  fetch(outputUrl, fetchOptions)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`failed to fetch the song segements sege-${segNum}`);
-      }
-      return response.arrayBuffer();
-    })
-    .then((buf) => {
-      if (
-        sourceBuffer.current?.buffered &&
-        !sourceBuffer.current.updating &&
-        mediaSource.current?.readyState
-      ) {
-        // console.log(segNum, "it got buffend");
-        sourceBuffer.current!.appendBuffer(buf);
-      }
-    })
 
-    .catch((err) => {
-      if (err.name === "AbortError") {
-        console.log(`the song segements sege-${segNum} fetching is aborted`);
-      } else {
-        console.error(`Error fetching segements sege-${segNum}`, err);
-      }
-    });
+  try {
+    const response = await fetch(outputUrl, fetchOptions);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch the song segment seg-${segNum}`);
+    }
+    const buf = await response.arrayBuffer();
+    if (
+      sourceBuffer.current?.buffered &&
+      !sourceBuffer.current.updating &&
+      mediaSource.current?.readyState
+    ) {
+      sourceBuffer.current!.appendBuffer(buf);
+    }
+
+    return buf;
+  } catch (err) {
+    const error = err as Error;
+    if (error.name === "AbortError") {
+      console.log(`The song segment seg-${segNum} fetching was aborted`);
+    } else {
+      console.error(`Error fetching segment seg-${segNum}:`, err);
+    }
+  }
 };
