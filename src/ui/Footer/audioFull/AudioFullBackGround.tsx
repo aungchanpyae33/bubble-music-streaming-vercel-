1653bbdd88ab3de31364eac15f28b50c968c01cd
aungchanpyae33 <khantlu24@gmail.com>
2025-03-20 19:00:23 +0630

    refactor : simplify color calculations in AudioFullBackGround and AlbumUpperBackground components

--- src/ui/Footer/audioFull/AudioFullBackGround.tsx
+++ src/ui/Footer/audioFull/AudioFullBackGround.tsx
@@ -15,12 +15,13 @@ export const Context = createContext<contextProps>({
 function AudioFullBackGround({ children }: Props) {
   const [bgValue, setBgValue] = useState<number[] | undefined>(undefined);
   const value = { bgValue, setBgValue };
-  const mainColor = bgValue
-    ? `rgb(${bgValue[0]},${bgValue[1]},${bgValue[2]})`
-    : "";
-  const bottomR = bgValue ? Math.round(Number(bgValue[0]) * 0.5) : "";
-  const bottomG = bgValue ? Math.round(Number(bgValue[1]) * 0.5) : "";
-  const bottomB = bgValue ? Math.round(Number(bgValue[2]) * 0.5) : "";
+  const R = bgValue ? bgValue[0] : "";
+  const G = bgValue ? bgValue[1] : "";
+  const B = bgValue ? bgValue[2] : "";
+  const bottomR = bgValue ? Math.round(Number(R) * 0.5) : "";
+  const bottomG = bgValue ? Math.round(Number(G) * 0.5) : "";
+  const bottomB = bgValue ? Math.round(Number(B) * 0.5) : "";
+  const mainColor = bgValue ? `rgb(${R},${G},${B})` : "";
   const bottomColor = `rgb(${bottomR},${bottomG},${bottomB})`;
   return (
     <>
--- src/ui/albumContainer/AlbumUpperBackground.tsx
+++ src/ui/albumContainer/AlbumUpperBackground.tsx
@@ -15,17 +15,18 @@ export const ContextAlbum = createContext<contextProps>({
 });
 function AlbumUpperBackground({ children }: Props) {
   const [bgValue, setBgValue] = useState<number[] | undefined>(undefined);
-  const bottomR = bgValue ? Math.round(Number(bgValue[0]) * 0.8) : "";
-  const bottomG = bgValue ? Math.round(Number(bgValue[1]) * 0.8) : "";
-  const bottomB = bgValue ? Math.round(Number(bgValue[2]) * 0.8) : "";
+  const R = bgValue ? bgValue[0] : "";
+  const G = bgValue ? bgValue[1] : "";
+  const B = bgValue ? bgValue[2] : "";
+  const bottomR = bgValue ? Math.round(Number(R) * 0.8) : "";
+  const bottomG = bgValue ? Math.round(Number(G) * 0.8) : "";
+  const bottomB = bgValue ? Math.round(Number(B) * 0.8) : "";
 
-  const bottomoverlayR = bgValue ? Math.round(Number(bgValue[0]) * 0.6) : "";
-  const bottomoverlayG = bgValue ? Math.round(Number(bgValue[1]) * 0.6) : "";
-  const bottomoverlayB = bgValue ? Math.round(Number(bgValue[2]) * 0.6) : "";
+  const bottomoverlayR = bgValue ? Math.round(Number(R) * 0.6) : "";
+  const bottomoverlayG = bgValue ? Math.round(Number(G) * 0.6) : "";
+  const bottomoverlayB = bgValue ? Math.round(Number(B) * 0.6) : "";
 
-  const mainColor = bgValue
-    ? `rgb(${bgValue[0]},${bgValue[1]},${bgValue[2]})`
-    : "";
+  const mainColor = bgValue ? `rgb(${R},${G},${B})` : "";
   const bottomColor = `rgb(${bottomR},${bottomG},${bottomB})`;
   const bottomoverlayColor = `rgba(${bottomoverlayR},${bottomoverlayG},${bottomoverlayB},0.9)`;
   console.log(mainColor, bottomColor);
