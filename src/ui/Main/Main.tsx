1dc62110ff5c4ac821ff62dc6ed64703e4da3319
aungchanpyae33 <khantlu24@gmail.com>
2025-03-20 18:47:49 +0630

    style : remove unnecessary padding from Main component for improved layout

--- src/ui/Main/Main.tsx
+++ src/ui/Main/Main.tsx
@@ -6,7 +6,7 @@ async function Main({ children }: { children: ReactNode }) {
   const deviceFromUserAgent = await DeviceCheck();
   return (
     <main
-      className={clsx("px-1 h-full flex-1 overflow-y-scroll", {
+      className={clsx(" h-full flex-1 overflow-y-scroll", {
         "md:pl-[70px]": deviceFromUserAgent === "desktop", //
         "pb-[70px]": deviceFromUserAgent === "mobile",
       })}
