10525ff987b240b61ae49c07dc7734c1df71243b
aungchanpyae33 <khantlu24@gmail.com>
2025-03-22 17:31:49 +0630

    fix : change logic for position fixed (prev position absolute)

--- src/lib/CustomHooks/TooltipOverflow.ts
+++ src/lib/CustomHooks/TooltipOverflow.ts
@@ -3,50 +3,54 @@ import {
   RefObject,
   SetStateAction,
   useLayoutEffect,
+  useRef,
   useState,
 } from "react";
 interface tooltipRefProp {
   toolTipRef: RefObject<HTMLDivElement | null>;
+  tooltipTargetRef: RefObject<HTMLDivElement | null>;
 }
 export interface tooltipState {
   show: boolean;
-  toolTipLeft: { left: string };
+  toolTipPosition: {};
 }
 const useTooltipOverflow = ({
   toolTipRef,
+  tooltipTargetRef,
 }: tooltipRefProp): [tooltipState, Dispatch<SetStateAction<tooltipState>>] => {
   const [tooltipShow, setTooltipShow] = useState<tooltipState>({
     show: false,
-    toolTipLeft: { left: "50%" },
+    toolTipPosition: {},
   });
+  const previousValue = useRef<number>(null);
   useLayoutEffect(() => {
-    const copyRef = toolTipRef.current;
-    if (tooltipShow.show && copyRef) {
-      const tooltipRect = copyRef.getBoundingClientRect();
-      const rightEdge = tooltipRect.right;
-      const leftEdge = tooltipRect.left;
-      if (rightEdge > window.document.body.offsetWidth) {
-        const data = rightEdge - window.document.body.offsetWidth;
+    const tooltipTargetEle = tooltipTargetRef.current;
+    const tooltipEle = toolTipRef.current;
+    if (tooltipShow.show && tooltipTargetEle && tooltipEle) {
+      const tooltipRect = tooltipTargetEle.getBoundingClientRect();
+      const innerWidth = window.innerWidth;
+      const tooltipRectTop = tooltipRect.top;
+      // conditional check run
+      if (previousValue.current !== innerWidth + tooltipRectTop) {
+        const goRect = tooltipEle.getBoundingClientRect();
+        previousValue.current = innerWidth + tooltipRectTop;
+        const data =
+          tooltipRect.left + tooltipRect.width / 2 - goRect.width / 2;
+        const positionX = Math.max(
+          8,
+          Math.min(data, innerWidth - goRect.width - 8)
+        );
+        const positionTop = tooltipRectTop - goRect.height;
+        const positionBottom = tooltipRect.bottom;
+        const positionY =
+          positionTop < 50 ? positionBottom + 8 : positionTop - 8;
         setTooltipShow((pre) => ({
           ...pre,
-          toolTipLeft: { left: `calc(50% - ${data}px)` },
-        }));
-      } else if (leftEdge < 0) {
-        const data = Math.abs(leftEdge);
-        setTooltipShow((pre) => ({
-          ...pre,
-          toolTipLeft: { left: `calc(50% + ${data}px)` },
+          toolTipPosition: { left: `${positionX}px`, top: `${positionY}px` },
         }));
       }
     }
-
-    return () => {
-      setTooltipShow((pre) => ({
-        ...pre,
-        toolTipLeft: { left: "50%" },
-      }));
-    };
-  }, [tooltipShow.show, toolTipRef]);
+  }, [tooltipShow.show, toolTipRef, tooltipTargetRef]);
 
   return [tooltipShow, setTooltipShow];
 };
