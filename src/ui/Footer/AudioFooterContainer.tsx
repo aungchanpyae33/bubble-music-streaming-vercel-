c3f23cdfd752dd1dc4864d1e4cfecb0eb598afea
aungchanpyae33 <khantlu24@gmail.com>
2025-03-25 23:05:41 +0630

    ui : act absolute element as parent with cursor not allowed

--- src/ui/Footer/AudioFooterContainer.tsx
+++ src/ui/Footer/AudioFooterContainer.tsx
@@ -16,40 +16,41 @@ function AudioFooterContainer({
   const { device } = useContext(ContextDevice);
   const { open, setOpen } = useContext(Context);
   return (
+    // <div className="cursor-not-allowed">
     <div
-      className={clsx("w-full h-full", {
-        "cursor-not-allowed": !url,
+      className={clsx("w-full h-[70px]", {
+        "absolute top-0 -translate-y-full left-0 overShort:static overShort:-translate-y-0 overShort:top-auto overShort:left-auto":
+          !open && device === "mobile",
+        " cursor-not-allowed": !url,
       })}
+      tabIndex={0}
+      onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          footerRef.current?.classList.toggle("z-50");
+          setOpen(!open);
+        }
+      }}
+      // to track initial click elemet , without this  check , if user click the button then hold and release the container that does not have e.stopP will trigger the parent onClick ,
+      onMouseDown={(e) => {
+        if (!url) return;
+        console.log(e.target);
+        initialRef!.current! = e.target as HTMLElement;
+      }}
+      onClick={(e) => {
+        if (!url) return;
+        if (e.target === initialRef.current) {
+          footerRef.current?.classList.toggle("z-50");
+          setOpen(!open);
+        }
+      }}
     >
       <div
         className={clsx(
-          "flex gap-4 sm:gap-5 md:gap-6 bg-white  lg:gap-10 justify-between w-full h-fit",
+          " w-full h-full flex gap-4 sm:gap-5 md:gap-6 bg-white  lg:gap-10 justify-between",
           {
-            "absolute top-0 -translate-y-full left-0 overShort:static overShort:-translate-y-0 overShort:top-auto overShort:left-auto":
-              !open && device === "mobile",
             " pointer-events-none": !url,
           }
         )}
-        tabIndex={0}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            footerRef.current?.classList.toggle("z-50");
-            setOpen(!open);
-          }
-        }}
-        // to track initial click elemet , without this  check , if user click the button then hold and release the container that does not have e.stopP will trigger the parent onClick ,
-        onMouseDown={(e) => {
-          if (!url) return;
-          console.log(e.target);
-          initialRef!.current! = e.target as HTMLElement;
-        }}
-        onClick={(e) => {
-          if (!url) return;
-          if (e.target === initialRef.current) {
-            footerRef.current?.classList.toggle("z-50");
-            setOpen(!open);
-          }
-        }}
       >
         {children}
       </div>
