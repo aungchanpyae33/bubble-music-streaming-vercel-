d061956907ca4c314cc490bb1429e5dc00f5faba
aungchanpyae33 <khantlu24@gmail.com>
2025-04-07 23:53:07 +0630

    fix: change overflow-y-scroll to overflow-y-auto for consistent scrolling behavior

--- src/ui/Main/Main.tsx
+++ src/ui/Main/Main.tsx
@@ -6,7 +6,7 @@ async function Main({ children }: { children: ReactNode }) {
   const deviceFromUserAgent = await DeviceCheck();
   return (
     <main
-      className={clsx(" h-full flex-1 overflow-y-scroll ", {
+      className={clsx(" h-full flex-1 overflow-y-auto ", {
         // padding pl-[70px] make extra bleed width overflow when children element is sticky top
         "md:ml-[70px]": deviceFromUserAgent === "desktop",
         "pb-[70px]": deviceFromUserAgent === "mobile",
