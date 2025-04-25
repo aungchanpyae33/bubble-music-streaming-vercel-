3e395b9500a39b178cc3c33af2f9d75c72fab5ca
aungchanpyae33 <khantlu24@gmail.com>
2025-04-25 11:18:32 +0630

    fix : focus trap and its children should be hide with motion presence

--- src/ui/Footer/audioFull/AudioCurImg.tsx
+++ src/ui/Footer/audioFull/AudioCurImg.tsx
@@ -1,18 +1,32 @@
-import { Context } from "@/lib/MediaSource/ContextMediaAudioFull";
 import clsx from "clsx";
-import { useContext } from "react";
-import AudioCu from "./AudioCu";
-import { AnimatePresence } from "motion/react";
+import Image from "next/image";
+import { ContextBackGround } from "./AudioFullBackGround";
+import { useContext, useRef } from "react";
+import useGetDominantColor from "@/lib/GetDominantColor";
 
 function AudioCurImg() {
-  const { open } = useContext(Context);
+  const { setBgValue } = useContext(ContextBackGround);
+  const imgRef = useRef<HTMLImageElement | null>(null);
+  const [isImageLoaded] = useGetDominantColor({ setBgValue, imgRef });
   return (
     <div
       className={clsx(
         " lg:h-[270px] h-[100%]  mx-auto lg:mx-0 aspect-square shrink-0 grow-0 fallbackAspect bg-[#343333] short:opacity-0 short:absolute  overflow-hidden relative"
       )}
     >
-      <AnimatePresence>{open && <AudioCu />}</AnimatePresence>
+      <Image
+        ref={imgRef}
+        src={
+          "https://s3.tebi.io/test1345/photo-1739467372234-2aba33f6b7ee.avif"
+        }
+        alt="this is image element"
+        fill
+      />
+      {isImageLoaded === "error" && (
+        <div className="absolute top-1/2 left-1/2 -translate-y-1/2 whitespace-nowrap    -translate-x-1/2">
+          some image to show image
+        </div>
+      )}
     </div>
   );
 }
--- src/ui/Footer/audioFull/AudioFull.tsx
+++ src/ui/Footer/audioFull/AudioFull.tsx
@@ -19,6 +19,7 @@ import AudioCurImg from "./AudioCurImg";
 import { X } from "lucide-react";
 import IconWrapper from "@/ui/general/IconWrapper";
 import clsx from "clsx";
+import MotionFull from "./MotionFull";
 function AudioFull({
   footerRef,
   url,
@@ -35,108 +36,117 @@ function AudioFull({
 
   CloseFunctoionForFull(open, setOpen, toggleRef, refFocus);
   return (
-    // <AnimatePresence>
-    //   {open && (
-    <FocusTrap
-      refFocus={refFocus}
-      mqAffectsChild={["(width >= 48rem)", "(width >= 64rem)"]}
+    <div
+      className={clsx(
+        "fixed inset-0 z-50 transition-transform duration-500 bg-[#0A0A0A]",
+        {
+          "translate-y-full": !open,
+          "translate-y-0": open,
+        }
+      )}
+      aria-hidden={!open}
     >
-      <AudioFullBackGround
-        className={clsx(
-          "fixed inset-0 z-50 flex flex-col items-center justify-center transition-transform ease-in-out duration-500 bg-[#0A0A0A]",
-          {
-            "translate-y-0": open,
-            "translate-y-full": !open,
-          }
-        )}
-        ref={refFocus}
-      >
-        <div className="mx-auto w-[90%] h-[55px] min-h-[55px] max-h-[55px]  flex items-center sticky top-0">
-          <button
-            className=" bg-transparent transition-colors  duration-200 hover:bg-[#333333] p-1 rounded-full flex items-center justify-center"
-            onClick={() => {
-              footerRef!.current!.classList.toggle("z-50");
-              toggleRef.current?.focus();
-              setOpen(!open);
-            }}
-          >
-            <IconWrapper size="large" Icon={X} />
-          </button>
-        </div>
-
-        <AudioFullInfoWrapper>
-          <AudioCurImg />
-        </AudioFullInfoWrapper>
-        <div className=" h-[20%] py-2  shrink-0  w-[90%]  inset-x-0 mx-auto sticky bottom-0">
-          <div className="audioFunctionContainer  flex  flex-col flex-1 h-full gap-y-10 items-center justify-center">
-            <div className="BottomContainer w-full static top-0 left-0 items-center ">
-              <div className="  w-full flex items-center relative">
-                <AudioSeekBar
-                  isFull={true}
-                  url={url}
-                  hideSliderInSmScreen={false}
-                  childrenFn={(value) => (
-                    <TimeIndicatorCur
-                      value={value}
-                      duration={duration}
-                      className="text-sm md:text-base  w-fit md:w-[5rem] text-start md:static md:left-auto md:bottom-auto md:translate-y-0  absolute left-0 bottom-0 translate-y-full"
-                    />
-                  )}
-                  duration={duration}
-                  key={url}
-                  className="hidden"
-                />
-                <TimeIndicatorDur
-                  duration={duration}
-                  className="text-sm md:text-base w-fit md:w-[5rem] text-end md:static md:right-auto md:bottom-auto md:translate-y-0 absolute right-0 bottom-0 translate-y-full "
-                />
-              </div>
-            </div>
-            <div className="upContainer w-full flex justify-center md:justify-between">
-              <div className="hidden md:flex w-[20%] max-w-[250px]  items-center">
-                hiello
-              </div>
-              <AudioFunctionButton>
-                {/* in jsx when use arrow and {} , react expect to return elemetn , if it does not have  return ,  implicitly returns void, or undefined, so, react think nothing to render  */}
-                {(playListArray) => (
-                  // return element
-                  <div
-                    className="flex flex-1  justify-center gap-5"
-                    onClick={(e) => e.stopPropagation()}
-                  >
-                    <AudioFunctionShuffle
-                      className="bg-pink-300 p-2  md:text-base text-2xl"
-                      urlProp={playListArray}
-                      url={url}
-                    />
-                    <AudioFunctionPre
-                      url={url}
-                      urlProp={playListArray}
-                      className="bg-pink-300 p-2  md:text-base text-2xl"
-                    />
-                    <ToggleButton
-                      className=" bg-red-300 md:text-base text-2xl "
-                      urlProp={playListArray}
-                    />
-                    <AudioFunctionNext
-                      url={url}
-                      urlProp={playListArray}
-                      className="bg-pink-300 p-2  md:text-base text-2xl"
-                    />
-                    <AudioFunctionRepeat className="bg-pink-300 p-2  md:text-base text-2xl" />
-                  </div>
+      <AnimatePresence>
+        {open && (
+          <MotionFull>
+            <FocusTrap
+              refFocus={refFocus}
+              mqAffectsChild={["(width >= 48rem)", "(width >= 64rem)"]}
+            >
+              <AudioFullBackGround
+                className={clsx(
+                  "w-full h-full flex flex-col items-center justify-center   "
                 )}
-              </AudioFunctionButton>
-              <div className="hidden md:flex  relative w-[20%] max-w-[250px] justify-end">
-                <Volume isFull={true} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </AudioFullBackGround>
-    </FocusTrap>
-    //   )}
-    // </AnimatePresence>
+                ref={refFocus}
+              >
+                <div className="mx-auto w-[90%] h-[55px] min-h-[55px] max-h-[55px]  flex items-center sticky top-0">
+                  <button
+                    className=" bg-transparent transition-colors  duration-200 hover:bg-[#333333] p-1 rounded-full flex items-center justify-center"
+                    onClick={() => {
+                      footerRef!.current!.classList.toggle("z-50");
+                      toggleRef.current?.focus();
+                      setOpen(!open);
+                    }}
+                  >
+                    <IconWrapper size="large" Icon={X} />
+                  </button>
+                </div>
+
+                <AudioFullInfoWrapper>
+                  <AudioCurImg />
+                </AudioFullInfoWrapper>
+                <div className=" h-[20%] py-2  shrink-0  w-[90%]  inset-x-0 mx-auto sticky bottom-0">
+                  <div className="audioFunctionContainer  flex  flex-col flex-1 h-full gap-y-10 items-center justify-center">
+                    <div className="BottomContainer w-full static top-0 left-0 items-center ">
+                      <div className="  w-full flex items-center relative">
+                        <AudioSeekBar
+                          isFull={true}
+                          url={url}
+                          hideSliderInSmScreen={false}
+                          childrenFn={(value) => (
+                            <TimeIndicatorCur
+                              value={value}
+                              duration={duration}
+                              className="text-sm md:text-base  w-fit md:w-[5rem] text-start md:static md:left-auto md:bottom-auto md:translate-y-0  absolute left-0 bottom-0 translate-y-full"
+                            />
+                          )}
+                          duration={duration}
+                          key={url}
+                          className="hidden"
+                        />
+                        <TimeIndicatorDur
+                          duration={duration}
+                          className="text-sm md:text-base w-fit md:w-[5rem] text-end md:static md:right-auto md:bottom-auto md:translate-y-0 absolute right-0 bottom-0 translate-y-full "
+                        />
+                      </div>
+                    </div>
+                    <div className="upContainer w-full flex justify-center md:justify-between">
+                      <div className="hidden md:flex w-[20%] max-w-[250px]  items-center">
+                        hiello
+                      </div>
+                      <AudioFunctionButton>
+                        {/* in jsx when use arrow and {} , react expect to return elemetn , if it does not have  return ,  implicitly returns void, or undefined, so, react think nothing to render  */}
+                        {(playListArray) => (
+                          // return element
+                          <div
+                            className="flex flex-1  justify-center gap-5"
+                            onClick={(e) => e.stopPropagation()}
+                          >
+                            <AudioFunctionShuffle
+                              className="bg-pink-300 p-2  md:text-base text-2xl"
+                              urlProp={playListArray}
+                              url={url}
+                            />
+                            <AudioFunctionPre
+                              url={url}
+                              urlProp={playListArray}
+                              className="bg-pink-300 p-2  md:text-base text-2xl"
+                            />
+                            <ToggleButton
+                              className=" bg-red-300 md:text-base text-2xl "
+                              urlProp={playListArray}
+                            />
+                            <AudioFunctionNext
+                              url={url}
+                              urlProp={playListArray}
+                              className="bg-pink-300 p-2  md:text-base text-2xl"
+                            />
+                            <AudioFunctionRepeat className="bg-pink-300 p-2  md:text-base text-2xl" />
+                          </div>
+                        )}
+                      </AudioFunctionButton>
+                      <div className="hidden md:flex  relative w-[20%] max-w-[250px] justify-end">
+                        <Volume isFull={true} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </AudioFullBackGround>
+            </FocusTrap>
+          </MotionFull>
+        )}
+      </AnimatePresence>
+    </div>
   );
 }
 
