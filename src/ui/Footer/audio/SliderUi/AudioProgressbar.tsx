29ba7b7e2c2ad8aeb44d68b357bcbb7de902b17e
aungchanpyae33 <khantlu24@gmail.com>
2025-05-13 12:16:48 +0630

    fix: adjust AudioProgressbar styling to use scale transformation for progress display

--- src/ui/Footer/audio/SliderUi/AudioProgressbar.tsx
+++ src/ui/Footer/audio/SliderUi/AudioProgressbar.tsx
@@ -7,11 +7,12 @@ function AudioProgressbar({
   value: number;
   progressRef: RefObject<HTMLDivElement | null>;
 }) {
+  const output = 100 - value;
   return (
     <div
-      className="bg-white  absolute top-0 left-0 h-full"
+      className="bg-white w-full left-0  absolute top-0 origin-left h-full"
       style={{
-        right: `${value}%`,
+        transform: `scaleX(${output}%)`,
       }}
       ref={progressRef}
     ></div>
