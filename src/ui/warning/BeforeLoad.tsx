a4af23f7c2579fe9dfe0f8add3eb148cd307c1ff
aungchanpyae33 <khantlu24@gmail.com>
2025-06-10 12:48:28 +0630

    feat: add BeforeLoad component to handle before unload events

---
+++ src/ui/warning/BeforeLoad.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { SongState, useSong } from "@/lib/zustand";
+import { useEffect } from "react";
+
+function BeforeLoad() {
+  const url = useSong(
+    (state: SongState) =>
+      Object.values(state.songCu as Record<string, string>)[0]
+  );
+  console.log(url);
+  useEffect(() => {
+    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+      if (!url) {
+        event.preventDefault();
+        return "";
+      }
+    };
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [url]);
+  return null;
+}
+
+export default BeforeLoad;
