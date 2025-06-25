b616a5a908778388c39a8bd5990d5f02bc423033
aungchanpyae33 <khantlu24@gmail.com>
2025-06-26 01:03:21 +0630

    refactor : change route handle getPlaylistData to the playlist and refactor the code and use actual data

---
+++ src/app/api/playlist/[slug]/route.ts
@@ -0,0 +1,37 @@
+export const runtime = "edge";
+import { getPlaylistSongs } from "@/database/data";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ slug: string }> }
+) {
+  const { slug } = await params;
+  if (!slug) {
+    return NextResponse.json(
+      { error: "Playlist id is required" },
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+  const { data, error } = await getPlaylistSongs(slug);
+
+  if (error || !data) {
+    return NextResponse.json(
+      { error: "Internal server error", details: error },
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+  const returnData = data && data[0];
+  if (!returnData) return null;
+
+  return NextResponse.json(returnData, {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
