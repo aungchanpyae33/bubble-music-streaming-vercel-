2e5c1b14cbad05972ed7c281932f89bfbdf4aca6
aungchanpyae33 <khantlu24@gmail.com>
2025-06-26 11:34:34 +0630

    refactor: restructure ArtistWrapper to add comma after element

--- src/ui/general/ArtistWrapper.tsx
+++ src/ui/general/ArtistWrapper.tsx
@@ -5,15 +5,16 @@ function ArtistWrapper({ artists }: { artists: artists[] }) {
   return (
     <>
       {artists.map((item, index) => (
-        <UnderLineLinkHover
-          key={item.id}
-          href={`/artist/${item.id}`}
-          prefetch={false}
-          className=" mr-1   leading-relaxed w-full truncate text-start"
-        >
-          {item.name}
-          {index < artists.length - 1 && ", "}
-        </UnderLineLinkHover>
+        <span key={item.id} className="inline">
+          <UnderLineLinkHover
+            href={`/artist/${item.id}`}
+            prefetch={false}
+            className="mr-1   leading-relaxed w-full truncate text-start"
+          >
+            {item.name}
+          </UnderLineLinkHover>
+          {index < artists.length - 1 && ","}
+        </span>
       ))}
     </>
   );
