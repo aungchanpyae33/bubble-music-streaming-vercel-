317377b7914d69196be46613e5b55041424a79ce
aungchanpyae33 <khantlu24@gmail.com>
2025-07-13 17:39:45 +0630

    fix: include uni_id parameter in removeSongsFromPlaylist function

--- src/actions/removeSongsFromPlaylist.ts
+++ src/actions/removeSongsFromPlaylist.ts
@@ -3,13 +3,15 @@
 import { createClient } from "@/database/server";
 export const removeSongsFromPlaylist = async (
   playlistId: string,
-  songId: string
+  songId: string,
+  uni_id: number
 ) => {
   try {
     const supabase = await createClient();
     const { data, error } = await supabase.rpc("delete_playlist_song", {
       p_id: playlistId,
       s_id: songId,
+      uni_id: uni_id,
     });
     return { data, error };
   } catch (error) {
