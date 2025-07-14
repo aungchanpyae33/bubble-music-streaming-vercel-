ae46902ce0ef2d462dbe044333d0ad44bc005334
aungchanpyae33 <khantlu24@gmail.com>
2025-07-15 01:52:25 +0630

    fix: refactor Queue component to improve rendering and remove unused might_repeat logic

--- src/ui/Queue/Queue.tsx
+++ src/ui/Queue/Queue.tsx
@@ -19,63 +19,67 @@ function Queue() {
     (state: currentSongPlaylist) => Object.values(state.playListArray)[0] || []
   ) as getSongsReturn;
   const queueRef = useRef<HTMLDivElement | null>(null);
-
   return (
     <div
       className={clsx(
-        "h-full  w-[20%] md:w-[25%] min-w-[250px] flex flex-col border border-neutral-200 border-opacity-15 border-y-0     max-w-[375px] overflow-y-auto"
+        "h-full [transform:translateZ(0)]   w-[20%] md:w-[25%] min-w-[250px] flex flex-col translate-3d  max-w-[375px] overflow-y-auto"
       )}
       ref={queueRef}
+      // [transform:translateZ(0)] for hardware acceleration , without this , it feels junky in chrome and some webkit browser
     >
       <PlaceHolderQueue queueRef={queueRef} />
       {playListArray &&
         playListArray.id &&
-        playListArray.songs.map((song) => (
-          <div
-            key={playListArray.might_repeat ? song.uni_id : song.id}
-            data-song-url={song.url}
-            className="flex py-1  hover:bg-[#333333] items-stretch
-          "
-          >
-            <ToggleElement
-              playlistSong={playListArray}
-              song={song}
-              className="w-[50px] "
-            />
+        playListArray.songs.map((song) => {
+          console.log(song.uni_id + song.id);
+          return (
             <div
-              className="flex-1 overflow-hidden
-              flex gap-2 items-center"
+              key={song.uni_id + song.id}
+              data-song-url={song.url}
+              className="flex py-1  hover:bg-[#333333] items-stretch
+          "
             >
-              <Image
-                src={
-                  "https://tebi.bubblemusic.dpdns.org/lee-hi/4-only/cover/photo_2025-05-23_14-51-24.jpg"
-                }
-                width={50}
-                height={50}
-                className=" rounded"
-                alt="test image"
-                priority={true}
+              <ToggleElement
+                playlistSong={playListArray}
+                song={song}
+                className="w-[50px] "
               />
+
               <div
-                className=" flex-1 truncate
-              "
+                className="flex-1 overflow-hidden
+              flex gap-2 items-center"
               >
-                {song.name}
-              </div>
-              <div className="w-[30px]">
-                <ContextInfoTrack
-                  id={playListArray!.id!}
-                  source={playListArray?.source || "none"}
-                  song={song}
+                <Image
+                  src={
+                    "https://tebi.bubblemusic.dpdns.org/lee-hi/4-only/cover/photo_2025-05-23_14-51-24.jpg"
+                  }
+                  width={50}
+                  height={50}
+                  className=" rounded"
+                  alt="test image"
+                  priority={false}
+                />
+                <div
+                  className=" flex-1 truncate
+              "
                 >
-                  <MoreOptionContext>
-                    <MoreOption targetElement={<TrackItemContainer />} />
-                  </MoreOptionContext>
-                </ContextInfoTrack>
+                  {song.name}
+                </div>
+                <div className="w-[30px]">
+                  <ContextInfoTrack
+                    id={playListArray!.id!}
+                    source={playListArray?.source || "none"}
+                    song={song}
+                  >
+                    <MoreOptionContext>
+                      <MoreOption targetElement={<TrackItemContainer />} />
+                    </MoreOptionContext>
+                  </ContextInfoTrack>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
     </div>
   );
 }
