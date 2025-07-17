b9b96438df8531d6aa2cfb419edf570c9120a183
aungchanpyae33 <khantlu24@gmail.com>
2025-07-17 17:08:09 +0630

    fix: integrate AnimatePresence and motion for improved animation in playlist rendering

--- src/ui/Queue/Queue.tsx
+++ src/ui/Queue/Queue.tsx
@@ -13,7 +13,8 @@ import { useRef } from "react";
 import PlaceHolderQueue from "./PlaceHolderQueue";
 import TrackItemContainer from "../trackComponent/TrackItemContainer";
 import ContextInfoTrack from "../trackComponent/ContextInfoTrack";
-
+import { AnimatePresence } from "motion/react";
+import { motion } from "motion/react";
 function Queue() {
   const playListArray = useRepeatAndCurrentPlayList(
     (state: currentSongPlaylist) => Object.values(state.playListArray)[0] || []
@@ -22,61 +23,68 @@ function Queue() {
   return (
     <div
       className={clsx(
-        "h-full [transform:translateZ(0)]   w-[20%] md:w-[25%] min-w-[250px] flex flex-col translate-3d  max-w-[375px] overflow-y-auto"
+        "h-full [transform:translateZ(0)]   w-[20%] md:w-[25%] min-w-[250px] flex flex-col translate-3d  max-w-[375px] overflow-y-auto overflow-x-hidden"
       )}
       ref={queueRef}
       // [transform:translateZ(0)] for hardware acceleration , without this , it feels junky in chrome and some webkit browser
     >
       <PlaceHolderQueue queueRef={queueRef} />
-      {playListArray &&
-        playListArray.id &&
-        playListArray.songs.map((song) => (
-          <div
-            key={song.uni_id + song.id}
-            data-song-id={song.uni_id + song.id}
-            className="flex py-1  hover:bg-[#333333] items-stretch
+      <AnimatePresence initial={false}>
+        {playListArray &&
+          playListArray.id &&
+          playListArray.songs.map((song) => (
+            <motion.div
+              layout="position"
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -10 }}
+              transition={{ duration: 0.3, ease: "easeOut" }}
+              key={song.uni_id + song.id}
+              data-song-id={song.uni_id + song.id}
+              className="flex py-1  hover:bg-[#333333] items-stretch
           "
-          >
-            <ToggleElement
-              playlistSong={playListArray}
-              song={song}
-              className="w-[50px] "
-            />
-
-            <div
-              className="flex-1 overflow-hidden
-              flex gap-2 items-center"
             >
-              <Image
-                src={
-                  "https://tebi.bubblemusic.dpdns.org/lee-hi/4-only/cover/photo_2025-05-23_14-51-24.jpg"
-                }
-                width={50}
-                height={50}
-                className=" rounded"
-                alt="test image"
-                priority={false}
+              <ToggleElement
+                playlistSong={playListArray}
+                song={song}
+                className="w-[50px] "
               />
+
               <div
-                className=" flex-1 truncate
-              "
+                className="flex-1 overflow-hidden
+              flex gap-2 items-center"
               >
-                {song.name}
-              </div>
-              <div className="w-[30px]">
-                <ContextInfoTrack
-                  id={playListArray!.id!}
-                  source={playListArray?.source || "none"}
-                  song={song}
+                <Image
+                  src={
+                    "https://tebi.bubblemusic.dpdns.org/lee-hi/4-only/cover/photo_2025-05-23_14-51-24.jpg"
+                  }
+                  width={50}
+                  height={50}
+                  className=" rounded"
+                  alt="test image"
+                  priority={false}
+                />
+                <div
+                  className=" flex-1 truncate
+              "
                 >
-                  <MoreOptionContext>
-                    <MoreOption targetElement={<TrackItemContainer />} />
-                  </MoreOptionContext>
-                </ContextInfoTrack>
+                  {song.name}
+                </div>
+                <div className="w-[30px]">
+                  <ContextInfoTrack
+                    id={playListArray!.id!}
+                    source={playListArray?.source || "none"}
+                    song={song}
+                  >
+                    <MoreOptionContext>
+                      <MoreOption targetElement={<TrackItemContainer />} />
+                    </MoreOptionContext>
+                  </ContextInfoTrack>
+                </div>
               </div>
-            </div>
-          </div>
-        ))}
+            </motion.div>
+          ))}
+      </AnimatePresence>
     </div>
   );
 }
