dfdf51dff815b7293bbdd0259954ac0d94a85ead
aungchanpyae33 <khantlu24@gmail.com>
2025-07-17 14:32:09 +0630

    fix: update outputCurrentIndexV2 to correctly type songsData and use proper identifiers for song retrieval

--- src/lib/CustomHooks/OutputCurrentIndexV2.ts
+++ src/lib/CustomHooks/OutputCurrentIndexV2.ts
@@ -1,12 +1,12 @@
 import { getSongsReturn } from "@/database/data";
 
 const outputCurrentIndexV2 = (
-  songsData: getSongsReturn,
+  songsData: getSongsReturn["songs"],
   id: string,
   uni_id: string
 ) => {
-  const currentIndex = songsData.songs.findIndex((song) => {
-    return song.url === id && song.uni_id === uni_id;
+  const currentIndex = songsData.findIndex((song) => {
+    return song.id === id && song.uni_id === uni_id;
   });
   return currentIndex;
 };
