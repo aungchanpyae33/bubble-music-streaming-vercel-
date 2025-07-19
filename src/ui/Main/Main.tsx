4b2475d421c9b42f50e9d31d289d7b094da4a7ca
aungchanpyae33 <khantlu24@gmail.com>
2025-07-19 11:13:34 +0630

    feat: enhance Main component styling for improved hardware acceleration and layout consistency

--- src/ui/Main/Main.tsx
+++ src/ui/Main/Main.tsx
@@ -6,10 +6,14 @@ async function Main({ children }: { children: ReactNode }) {
   const deviceFromUserAgent = await DeviceCheck();
   return (
     <main
-      className={clsx(" h-full flex-1 overflow-y-auto ", {
-        // padding pl-[70px] make extra bleed width overflow when children element is sticky top
-        "md:ml-[70px]": deviceFromUserAgent === "desktop",
-      })}
+      className={clsx(
+        " h-full flex-1 overflow-y-auto [transform:translateZ(0)] ",
+        // [transform:translateZ(0)] for hardware acceleration , without this , it feels junky in chrome and some webkit browser
+        {
+          // padding pl-[70px] make extra bleed width overflow when children element is sticky top
+          "md:ml-[70px]": deviceFromUserAgent === "desktop",
+        }
+      )}
     >
       {children}
     </main>
