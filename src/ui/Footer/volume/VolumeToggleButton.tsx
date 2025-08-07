1d3eace4732f53c67eafdf2a786880e9d9d61625
aungchanpyae33 <khantlu24@gmail.com>
2025-08-08 00:24:17 +0630

    fix: update VolumeToggleButton to manage visibility based on open state

--- src/ui/Footer/volume/VolumeToggleButton.tsx
+++ src/ui/Footer/volume/VolumeToggleButton.tsx
@@ -2,11 +2,17 @@ import IconWrapper from "@/ui/general/IconWrapper";
 import { Volume2, VolumeX } from "lucide-react";
 import { useContext } from "react";
 import { volumeContext } from "./ContextVolume";
+import clsx from "clsx";
 
 function VolumeToggleButton({ value }: { value: number }) {
-  const { setOpen } = useContext(volumeContext);
+  const { open, setOpen } = useContext(volumeContext);
   return (
-    <button className="lg:hidden" onClick={() => setOpen(true)}>
+    <button
+      className={clsx("lg:hidden", {
+        hidden: open,
+      })}
+      onClick={() => setOpen(true)}
+    >
       {" "}
       {value === 100 ? (
         <IconWrapper size="small" Icon={VolumeX} />
