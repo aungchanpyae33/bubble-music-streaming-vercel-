df14e6862316a5764052732743591005c18aae34
aungchanpyae33 <khantlu24@gmail.com>
2025-08-08 00:26:38 +0630

    fix : set volume on page load

--- src/ui/Footer/audio/AudioWrapper.tsx
+++ src/ui/Footer/audio/AudioWrapper.tsx
@@ -1,5 +1,10 @@
-import { SongFunctionState, useSongFunction } from "@/lib/zustand";
-import { RefObject } from "react";
+import {
+  SongFunctionState,
+  useSongFunction,
+  useVolumeValue,
+  VolumeValueState,
+} from "@/lib/zustand";
+import { RefObject, useEffect, useRef } from "react";
 
 function AudioWrapper({
   dataAudio,
@@ -8,17 +13,32 @@ function AudioWrapper({
   dataAudio: RefObject<HTMLAudioElement | null>;
   url: string;
 }) {
+  const attchVol = useRef(false);
   const Isplay = useSongFunction(
     (state: SongFunctionState) =>
       Object.keys(state.Isplay as Record<string, boolean>)[0]
   );
-  // );
+  const value = useVolumeValue((state: VolumeValueState) =>
+    !attchVol.current ? state.value : undefined
+  );
+  useEffect(() => {
+    if (!attchVol.current) {
+      attchVol.current = true;
+    }
+  }, []);
+
   return (
     <audio
       ref={dataAudio}
       className="hidden"
       hidden
-      onLoadedMetadata={(e) => Isplay && e.currentTarget.play()}
+      onLoadedMetadata={(e) => {
+        if (value !== undefined) {
+          const defaultVol = 1 - value / 100;
+          e.currentTarget.volume = defaultVol;
+        }
+        Isplay && e.currentTarget.play();
+      }}
     ></audio>
   );
 }
