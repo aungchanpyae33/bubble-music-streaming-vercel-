55a1dd7a3e8c1765f736141aedbac1eaa0d901d3
aungchanpyae33 <khantlu24@gmail.com>
2025-08-07 11:50:54 +0630

    fix: improve TimeFormat function to handle NaN and undefined values correctly

--- src/lib/TimeFormat.ts
+++ src/lib/TimeFormat.ts
@@ -1,7 +1,8 @@
 export const TimeFormat = (sec: number | undefined) => {
-  if (sec) {
-    const minutes = Math.floor(sec / 60);
-    const seconds = Math.round(sec % 60);
+  if (typeof sec === "number" && !isNaN(sec)) {
+    const totalSeconds = Math.round(sec); // round first
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
 
     const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
     const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
