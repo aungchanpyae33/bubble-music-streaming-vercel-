eef3de1c2f39a4e4eb853b4f35b8ad5d46095cf3
aungchanpyae33 <khantlu24@gmail.com>
2025-08-13 12:51:18 +0630

    edit : toggle commit for bundle analyze

--- next.config.mjs
+++ next.config.mjs
@@ -25,3 +25,35 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// import withBundleAnalyzer from "@next/bundle-analyzer";
+// const nextConfig = {
+//   experimental: {
+//     ppr: true,
+//   },
+
+//   // reactStrictMode: false,
+//   images: {
+//     formats: ["image/avif", "image/webp"],
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "s3.tebi.io",
+//         port: "",
+//         pathname: "/test1345/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "tebi.bubblemusic.dpdns.org",
+//         port: "",
+//         pathname: "/**",
+//       },
+//     ],
+//   },
+// };
+// const analyzer = withBundleAnalyzer({
+//   enabled: process.env.ANALYZE === "true",
+// });
+
+// export default analyzer(nextConfig);
