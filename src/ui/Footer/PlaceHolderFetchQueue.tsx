fcf8ba959d3300aacee8e6e095510675ea85e674
aungchanpyae33 <khantlu24@gmail.com>
2025-08-17 18:59:09 +0630

    refactor: update fetch logic to use getSimilarSongQueueClient and streamline song handling

--- src/ui/Footer/PlaceHolderFetchQueue.tsx
+++ src/ui/Footer/PlaceHolderFetchQueue.tsx
@@ -1,4 +1,4 @@
-import { getSimilarSongQueue } from "@/database/client-data";
+import { getSimilarSongQueueClient } from "@/database/client-data";
 import { generateUUID } from "@/lib/GenerateUUID";
 import {
   currentAddToQueueAction,
@@ -20,44 +20,37 @@ function PlaceHolderFetchQueue() {
   const currentAddToQueue = useRepeatAndCurrentPlayList(
     (state: currentAddToQueueAction) => state.currentAddToQueue
   );
-  const { id, name } = useSong(
-    (state: SongState) => state.songCu
-  ) as SongDetail;
+  const { song_id } = useSong((state: SongState) => state.songCu) as SongDetail;
   useEffect(() => {
-    console.log(
-      FetchSongsListId,
-      FetchSongsListIdPreRef.current,
-      FetchSongsListId
-    );
-    // (async () => {
-    //   if (
-    //     FetchSongsListId &&
-    //     FetchSongsListIdPreRef.current !== FetchSongsListId
-    //   ) {
-    //     FetchSongsListIdPreRef.current = FetchSongsListId;
-    //     if (abortController.current) {
-    //       // it will abort when it use with signal
-    //       abortController.current.abort();
-    //       abortController.current = null;
-    //     }
-    //     abortController.current = new AbortController();
-    //     const { data, error } = await getSimilarSongQueue(id, abortController);
-    //     if (!data || error) return null;
-    //     console.log("run");
-    //     const updatedSongs = data.map((song) => ({
-    //       ...song,
-    //       uni_id: generateUUID(),
-    //     }));
-    //     if (
-    //       FetchSongsListId &&
-    //       FetchSongsListIdPreRef.current === FetchSongsListId
-    //     ) {
-    //       console.log("fin");
-    //       currentAddToQueue(updatedSongs);
-    //     }
-    //   }
-    // })();
-  }, [id, FetchSongsListId, currentAddToQueue]);
+    (async () => {
+      if (
+        FetchSongsListId &&
+        FetchSongsListIdPreRef.current !== FetchSongsListId
+      ) {
+        FetchSongsListIdPreRef.current = FetchSongsListId;
+        if (abortController.current) {
+          // it will abort when it use with signal
+          abortController.current.abort();
+          abortController.current = null;
+        }
+        abortController.current = new AbortController();
+        const { data, error } = await getSimilarSongQueueClient(
+          song_id,
+          abortController
+        );
+        if (!data || error) return null;
+        if (!data || error) return;
+        if (
+          FetchSongsListId &&
+          FetchSongsListIdPreRef.current === FetchSongsListId
+        ) {
+          const { songs } = data;
+          if (!songs) return null;
+          currentAddToQueue(songs, songs.idArray);
+        }
+      }
+    })();
+  }, [song_id, FetchSongsListId, currentAddToQueue]);
   return null;
 }
 
