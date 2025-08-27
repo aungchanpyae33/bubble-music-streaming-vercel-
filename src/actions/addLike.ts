e1a3ab87a07400045ea481a5d2a2d91e5badc806
aungchanpyae33 <khantlu24@gmail.com>
2025-08-27 20:01:55 +0630

    fix: wrap database calls in try-catch for error handling in addLike and removeLike functions

--- src/actions/addLike.ts
+++ src/actions/addLike.ts
@@ -1,11 +1,14 @@
 "use server";
 import { createClient } from "@/database/server";
-import { redirect } from "next/navigation";
 
 export const addLike = async (songId: string) => {
-  const supabase = await createClient();
-  const { error } = await supabase.rpc("addlike", {
-    song_id: songId,
-  });
-  return { error };
+  try {
+    const supabase = await createClient();
+    const { error } = await supabase.rpc("addlike", {
+      song_id: songId,
+    });
+    return { error };
+  } catch (error) {
+    return { error };
+  }
 };
--- src/actions/removeLike.ts
+++ src/actions/removeLike.ts
@@ -3,10 +3,13 @@
 import { createClient } from "@/database/server";
 
 export const removeLike = async (songId: string) => {
-  const supabase = await createClient();
-  const { error } = await supabase.rpc("removelike", {
-    song_id: songId,
-  });
-
-  return { error };
+  try {
+    const supabase = await createClient();
+    const { error } = await supabase.rpc("removelike", {
+      song_id: songId,
+    });
+    return { error };
+  } catch (error) {
+    return { error };
+  }
 };
