15fc3472a6ef6b1ee6b575d675db139e595b21a2
aungchanpyae33 <khantlu24@gmail.com>
2025-09-03 17:31:41 +0630

    Revert "ui : add profile  page"
    This reverts commit fb17acab193e67f4dc7ad3f3923199eb82ee67a8.

---
+++ src/app/(root)/profile/page.tsx
@@ -0,0 +1,12 @@
+function page() {
+  return (
+    <div className=" bg-red-500 mt-32 relative">
+      this is profile
+      <div className=" absolute z-50 bg-green-700 top-0 -translate-y-full left-0 -translate-x-full">
+        hello
+      </div>
+    </div>
+  );
+}
+
+export default page;
--- src/ui/general/optionBox/SongListContainerOption.tsx
+++ src/ui/general/optionBox/SongListContainerOption.tsx
@@ -4,14 +4,12 @@ import ArtistContainerOption from "@/ui/artist/ArtistContainerOption";
 import { SongListContext } from "@/ui/playlist/playlistOption/ContextSongListContainer";
 import PlaylistContainerOption from "@/ui/playlist/playlistOption/PlaylistContainerOption";
 import { useContext } from "react";
-import ProfileOption from "./ProfileOption";
 
 function SongListContainerOption() {
   const { type } = useContext(SongListContext);
   if (type === "playlist") return <PlaylistContainerOption />;
   if (type === "album") return <AlbumContainerOption />;
   if (type === "artist") return <ArtistContainerOption />;
-  if (type === "profile") return <ProfileOption />;
   return null;
 }
 
