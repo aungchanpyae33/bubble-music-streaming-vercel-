9705056d4ce4601b5ed4c063985369de4379a0b4
aungchanpyae33 <khantlu24@gmail.com>
2025-09-03 17:36:21 +0630

    ui : receive props class-name  for profile page case

--- src/ui/general/ListContainerOption/ListContainer.tsx
+++ src/ui/general/ListContainerOption/ListContainer.tsx
@@ -1,11 +1,14 @@
 import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
-function ListContainer({ children }: { children: ReactNode }) {
-  return (
-    <div className=" border my-3 rounded border-gray-200/20 w-fit gap-x-6 p-3 h-[80px] flex items-center ">
-      {children}
-    </div>
-  );
+const baseStyle =
+  " border my-3 rounded border-gray-200/20 w-fit gap-x-6 p-3 h-[80px] flex items-center";
+interface ListContainerProps extends React.ComponentProps<"div"> {
+  children: ReactNode;
+  className?: string;
+}
+function ListContainer({ children, className }: ListContainerProps) {
+  return <div className={twMerge(baseStyle, className)}>{children}</div>;
 }
 
 export default ListContainer;
