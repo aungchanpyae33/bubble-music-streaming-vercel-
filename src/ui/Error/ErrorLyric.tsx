d5e69e8034007e16ea8c5a3f860b2e5b5075e4ec
aungchanpyae33 <khantlu24@gmail.com>
2025-09-05 17:51:04 +0630

    ui : change style

--- src/ui/Error/ErrorLyric.tsx
+++ src/ui/Error/ErrorLyric.tsx
@@ -1,13 +1,10 @@
-import Image from "next/image";
 import Error from "../icon/Error";
 
 function ErrorLyric() {
   return (
-    <div className="flex flex-col gap-5 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 items-center justify-center">
-      <Error height={250} width={250} />
-      <span className=" whitespace-nowrap">
-        သီချင်းစာသားကို ရယူရာတွင် အခက်အခဲရှိခဲ့ပါသည်
-      </span>
+    <div className="flex flex-col gap-5 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full items-center justify-center">
+      <Error height={"70%"} width={"70%"} />
+      <span className="">သီချင်းစာသားကို ရယူရာတွင် အခက်အခဲရှိခဲ့ပါသည်</span>
     </div>
   );
 }
--- src/ui/NoExist/NoExistLyric.tsx
+++ src/ui/NoExist/NoExistLyric.tsx
@@ -2,11 +2,9 @@ import NoExist from "../icon/NoExist";
 
 function NoExistLyric() {
   return (
-    <div className="flex flex-col gap-5 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 items-center justify-center">
-      <NoExist width={250} height={250} />
-      <span className=" whitespace-nowrap">
-        ဒီသီချင်းအတွက် သီချင်းစာသား မရှိသေးပါ
-      </span>
+    <div className="flex flex-col gap-5 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full  items-center justify-center">
+      <NoExist width={"70%"} height={"70%"} />
+      <span className="">ဒီသီချင်းအတွက် သီချင်းစာသား မရှိသေးပါ</span>
     </div>
   );
 }
