10a44b525ea32619f9bcb4bea2a7c1b79f8d0226
aungchanpyae33 <khantlu24@gmail.com>
2025-09-08 15:17:33 +0630

    remove : drop inline bundle svg icon , as it can cause the laggy animation in low end mobile device

--- src/ui/NoExist/NoExistLyric.tsx
+++ src/ui/NoExist/NoExistLyric.tsx
@@ -1,10 +1,7 @@
-import NoExist from "../icon/NoExist";
-
 function NoExistLyric() {
   return (
-    <div className="flex flex-col gap-5 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full  items-center justify-center">
-      <NoExist width={"70%"} height={"70%"} />
-      <span className="">ဒီသီချင်းအတွက် သီချင်းစာသား မရှိသေးပါ</span>
+    <div className="flex absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full  items-center justify-center">
+      <span className=" ">ဒီသီချင်းအတွက် သီချင်းစာသား မရှိသေးပါ</span>
     </div>
   );
 }
