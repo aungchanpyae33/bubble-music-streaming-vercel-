c7367a24669cbc7f1103adeeba51d83554563422
aungchanpyae33 <khantlu24@gmail.com>
2025-09-08 15:21:22 +0630

    ui : drop layout animation with motion.div as it does lag in mobile low end

--- src/ui/Footer/audioFull/AudioFullInfoWrapper.tsx
+++ src/ui/Footer/audioFull/AudioFullInfoWrapper.tsx
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 import LyricContainer from "./LyricContainer";
 import QueueFull from "@/ui/Queue/QueueFull";
 import CloseShowBlockBtn from "./CloseShowBlockBtn";
+import Image from "next/image";
 function AudioFullInfoWrapper({ children }: { children: React.ReactNode }) {
   const showBlock = useShowBlock((state: ShowBlock) => state.showBlock);
   const setShowBlock = useShowBlock(
@@ -17,69 +18,41 @@ function AudioFullInfoWrapper({ children }: { children: React.ReactNode }) {
     };
   }, [setShowBlock]);
   return (
-    <div className=" mx-auto   w-[90%]  flex-1 flex     relative overflow-hidden ">
-      <motion.div
-        layout
+    <div className=" mx-auto   w-[96%] md:w-[80%] lg:w-[90%]  flex-1 flex relative overflow-hidden ">
+      <div
         className={clsx(
-          "lg:items-end will-change-transform relative lg:w-[50%]  gap-2 lg:gap-4 lg:justify-start overflow-hidden   flex   lg:flex-row   w-full",
+          "lg:items-end will-change-transform relative lg:w-[50%]  gap-2 lg:gap-4 lg:justify-start overflow-hidden flex-col flex   lg:flex-row items-center transition-[transform,opacity] duration-300 lg:scale-100 lg:translate-y-0 lg:opacity-100 justify-center w-full",
           {
-            "flex-col": !showBlock.open,
-            "justify-start items-start": showBlock.open,
-            "items-center justify-center": !showBlock.open,
+            " scale-100 translate-y-0  opacity-100": !showBlock.open,
+            " scale-90 -translate-y-10 opacity-0": showBlock.open,
           }
         )}
       >
-        <motion.div
-          layout
+        <div
           className={clsx(
-            " flex items-center shrink-0 grow-0  overflow-hidden  lg:h-auto",
-            {
-              "h-[80%] max-h-[90vw]": !showBlock.open,
-              "h-[60px]": showBlock.open,
-            }
+            " flex items-center shrink-0 grow-0  overflow-hidden h-[80%] max-h-[90vw]  lg:h-auto"
           )}
         >
           {children}
-        </motion.div>
-        <motion.div
-          layout
-          className={clsx("min-h-[60px] max-w-fit  flex  items-center", {
-            "flex-1": showBlock.open,
-            "self-start lg:self-auto": !showBlock.open,
-          })}
+        </div>
+        <div
+          className={clsx(
+            "min-h-[60px] max-w-fit  flex  items-center self-start lg:self-auto"
+          )}
         >
-          <motion.div
-            layout
-            className="flex items-start  p-1 justify-center flex-col "
-          >
-            <p
-              className={clsx(
-                " text-zinc-100  text-xl lg:text-2xl transition-transform origin-left duration-500",
-                {
-                  " scale-90 lg:scale-100 ": showBlock.open,
-                  "scale-100 ": !showBlock.open,
-                }
-              )}
-            >
+          <div className="flex items-start  p-1 justify-center flex-col ">
+            <p className={clsx(" text-zinc-100  text-xl lg:text-2xl")}>
               Supanova
             </p>
-            <p
-              className={clsx(
-                " text-zinc-100  lg:text-xl text-base transition-transform origin-left  duration-500",
-                {
-                  " scale-90 lg:scale-100 ": showBlock.open,
-                  "scale-100 ": !showBlock.open,
-                }
-              )}
-            >
+            <p className={clsx(" text-zinc-100  lg:text-xl text-base")}>
               aspea
             </p>
-          </motion.div>
-        </motion.div>
-      </motion.div>
+          </div>
+        </div>
+      </div>
       <div
         className={clsx(
-          " bg-black  left-auto  lg:w-[50%] w-full   px-[36px] inset-0 top-[60px] absolute grid grid-rows-[36px_1fr_36px] transition-[opacity,transform] duration-500",
+          " bg-black will-change-transform  left-auto lg:grid-rows-[36px_1fr_36px] lg:w-[50%] w-full lg:top-[60px] inset-0  absolute grid grid-rows-[60px_1fr_36px] transition-[opacity,transform] duration-500",
           {
             "translate-y-0": showBlock.open,
             "translate-y-full": !showBlock.open,
@@ -87,30 +60,36 @@ function AudioFullInfoWrapper({ children }: { children: React.ReactNode }) {
         )}
         tabIndex={-1}
       >
-        <LyricPaddingBlock />
-        {showBlock.open && <CloseShowBlockBtn />}
-        <div className=" relative w-full h-full overflow-hidden ">
-          <AnimatePresence>
-            {showBlock.open && (
-              <motion.div
-                exit={{ opacity: 0.99 }}
-                transition={{ duration: 0.5 }}
-                className={clsx(
-                  " w-[200%] inset-0 transition-transform duration-500 ease-in-out flex will-change-transform   absolute",
-                  {
-                    "-translate-x-1/2 -left-1": showBlock.type === "queue",
-                  }
-                )}
-              >
-                <LyricContainer type={showBlock.type} />
-                <QueueFull />
-              </motion.div>
-            )}
-          </AnimatePresence>
+        <div
+          className={clsx(
+            "flex lg:hidden bg-transparent   items-center w-full"
+          )}
+        >
+          <div className={clsx("relative size-[60px]")}>
+            <Image
+              src={
+                "https://tebi.bubblemusic.dpdns.org/lee-hi/4-only/cover/photo_2025-05-23_14-51-24.jpg"
+              }
+              alt="this is image element"
+              fill
+            />
+          </div>
+          <p>hello</p>
         </div>
 
-        <LyricPaddingBlock />
+        <LyricPaddingBlock className="hidden lg:block" />
+        {showBlock.open && <CloseShowBlockBtn />}
+        <AnimatePresence mode="wait">
+          {showBlock.type === "lyric" && showBlock.open && (
+            <LyricContainer type={showBlock.type} key={"lyric"} />
+          )}
+          {showBlock.type === "queue" && showBlock.open && (
+            <QueueFull key={"queue"} />
+          )}
+        </AnimatePresence>
       </div>
+
+      <LyricPaddingBlock />
     </div>
   );
 }
--- src/ui/Footer/audioFull/LyricPaddingBlock.tsx
+++ src/ui/Footer/audioFull/LyricPaddingBlock.tsx
@@ -1,6 +1,8 @@
+import { twMerge } from "tailwind-merge";
+
 interface LyricPaddingBlockProps extends React.ComponentProps<"div"> {}
 function LyricPaddingBlock({ className, children }: LyricPaddingBlockProps) {
-  return <div className={`${className}`}>{children}</div>;
+  return <div className={twMerge(className)}>{children}</div>;
 }
 
 export default LyricPaddingBlock;
