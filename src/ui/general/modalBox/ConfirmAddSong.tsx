bfde04cf5a4976c74eb01d1454d0fd52e9080708
aungchanpyae33 <khantlu24@gmail.com>
2025-09-27 23:48:55 +0630

    fix : get cover_url to use in mutation

--- src/ui/general/modalBox/ConfirmAddSong.tsx
+++ src/ui/general/modalBox/ConfirmAddSong.tsx
@@ -1,9 +1,12 @@
 "use client";
 import useAddSongMutate from "@/lib/CustomHooks/mutation/AddSongMutate";
 import {
+  addSongsToPlaylistProps,
   isSongExist,
   songExist,
   songExistAction,
+  songsToPlaylist,
+  useAddSongsToPlaylist,
   useIsExistSongs,
 } from "@/lib/zustand";
 import { useEffect } from "react";
@@ -12,10 +15,13 @@ function ConfirmAddSong() {
   const { playlistId, songId } = useIsExistSongs(
     (state: isSongExist) => state.isSongExist
   ) as songExist;
+  const { cover_url } = useAddSongsToPlaylist(
+    (state: songsToPlaylist) => state.songsToPlaylist
+  ) as addSongsToPlaylistProps;
   const setIsSongExist = useIsExistSongs(
     (state: songExistAction) => state.setIsSongExist
   );
-  const mutation = useAddSongMutate(playlistId);
+  const mutation = useAddSongMutate(playlistId, cover_url);
   function handleAdd() {
     mutation.mutate({
       playlistId,
