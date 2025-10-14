271f32b1b45823657eb66d230d2802f81eaeef33
aungchanpyae33 <khantlu24@gmail.com>
2025-10-14 21:48:23 +0630

    add : add cover_url change feature when remove songs from playlist.

--- src/lib/CustomHooks/mutation/RemoveSongMutate.ts
+++ src/lib/CustomHooks/mutation/RemoveSongMutate.ts
@@ -1,4 +1,5 @@
 import { removeSongsFromPlaylist } from "@/actions/removeSongsFromPlaylist";
+import { listSongsSection, navbarList } from "@/database/data";
 import { ContextMoreOption } from "@/ui/trackComponent/MoreOptionContext";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useContext } from "react";
@@ -11,6 +12,52 @@ const useRemoveSongMutate = (id: string) => {
       const { data, error } = queryData;
       if (!data || error) return;
       if (!error && data) {
+        const { data: playlistData } = queryClient.getQueryData<{
+          data: { songs: listSongsSection };
+          error: null | string;
+        }>(["playlist", id])!;
+
+        if (!playlistData) return;
+        const { songs } = playlistData;
+        if (!songs) return;
+        const { idArray, songs: scope_songs } = songs;
+        const didFirstIdRemoving = idArray[0] === variables.id;
+        if (didFirstIdRemoving) {
+          queryClient.setQueryData(
+            ["user-library"],
+            (
+              oldData:
+                | {
+                    data: {
+                      userLib: Record<string, navbarList> & {
+                        idArray: string[];
+                      };
+                    };
+                    error: null | string;
+                  }
+                | undefined
+            ) => {
+              if (!oldData) return oldData;
+
+              const cover_url = idArray[1]
+                ? scope_songs[idArray[1]].cover_url
+                : null;
+              const updatedUserLib = {
+                ...oldData.data.userLib,
+                [id]: {
+                  ...oldData.data.userLib[id],
+                  cover_url,
+                },
+              };
+              return {
+                data: {
+                  userLib: updatedUserLib,
+                },
+                error: null,
+              };
+            }
+          );
+        }
         queryClient.setQueryData(["playlist", id], { data, error: null });
       }
       setShow(false);
