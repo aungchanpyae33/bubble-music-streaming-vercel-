d22d1f44d8da2fb0e894b77d27802975112ee608
aungchanpyae33 <khantlu24@gmail.com>
2025-11-30 13:18:43 +0630

    refactor : add comment to code and refactor the calculate of x to follow the resize when item is not in view port
    the problem is window width as it is not accurate in resize , use clientWidth for calculate X position

--- src/lib/CustomHooks/ToggleContentPosition.ts
+++ src/lib/CustomHooks/ToggleContentPosition.ts
@@ -17,13 +17,20 @@ export const useToggleContentPosition = ({
     transform: "translate(0px, 0px)",
   });
   const initialHeightRef = useRef<number | null>(null);
+
+  // the reason i use use layout effect is obvious , that to run this effect before the layout pain , because i need to the content width , height , etc before paint to calculate the position of its to render
   useLayoutEffect(() => {
     const updatePosition = () => {
+      // parent is vertical icon cantainer
       const parentEl = parentRef.current;
+      // container is content
       const containerEl = containerRef.current;
       if (!parentEl || !containerEl) return;
-      const windowHeight = window.innerHeight;
-      const windowWidth = window.innerWidth;
+
+      // always use clientWidth and clientHeight when accurate positin is needed like for toggleTip(fixed position)
+      // previouse windowidth is not accurate in windowWidth including scrollbar width and aslo inconsistent in resize
+      const viewportWidth = document.documentElement.clientWidth;
+      const viewportHeight = document.documentElement.clientHeight;
       const targetRect = parentEl.getBoundingClientRect();
       const containerRect = containerEl.getBoundingClientRect();
       const targetTop = targetRect.top;
@@ -35,29 +42,72 @@ export const useToggleContentPosition = ({
       if (initialHeightRef.current === null) {
         initialHeightRef.current = containerRect.height;
       }
+      // calculate how many space is left in below when position in top is not available
+      const spaceBelow = viewportHeight - targetTop;
+      //calculate how many space is left in right when postion is left is not available
+      const spaceRight = viewportWidth - targetLeft;
 
-      const spaceBelow = windowHeight - targetTop;
+      // i wll add comment in modify , new , previouse code to explain what is it for future as in later i forget how it works in some component
 
+      // how x component is calculate
+      // it has  check -> first check is space available in left, second check is it is  avalile in left go option to left
       const x = (() => {
-        if (targetLeft < containerWidth) {
-          return targetRight;
-        } else {
-          if (targetLeft > windowWidth) {
-            return windowWidth - containerWidth;
+        const notSpaceAvailableInLeft = targetLeft < containerWidth;
+        if (notSpaceAvailableInLeft) {
+          // right side option mind
+
+          //  if not left space in left space , check it does it also space in right?
+          const notSpaceAvailableInRight = containerWidth > spaceRight;
+          if (notSpaceAvailableInRight) {
+            // it not space available in right too , then return targetLeft with reduce version
+            return targetLeft - (containerWidth - spaceRight);
+          } else {
+            return targetLeft;
           }
-          return targetLeft - containerWidth;
+        } else {
+          //in left side option mind
+
+          // in resize , this check does target item is not in viewport or not for resize behavior
+          const isTargetItemNotInViewPort = viewportWidth - targetRight <= 0;
+          if (isTargetItemNotInViewPort) {
+            // then return the targetLeft with reduce version
+            return targetLeft - (containerWidth - spaceRight);
+          } else {
+            // if target item is in viewport still , then return relative positino
+            // why i use targetLeft instead of right in this case is that not to cover the vertical icon
+            return targetLeft - containerWidth;
+          }
         }
       })();
 
+      // how y component is calculate
+      // it has   check -> first check is space available in top, second check is if it is avalile in top go option to top
       const y = (() => {
-        if (targetTop < containerHeight) {
-          return containerHeight > spaceBelow
-            ? targetTop - (containerHeight - spaceBelow)
-            : targetTop;
+        const notSpaceAvailableInTop = targetTop < containerHeight;
+        if (notSpaceAvailableInTop) {
+          //  bottom side option mind
+
+          //  if not left space in top space , check it does it also space in bottom?
+          const notSpaceAvailableInBottom = containerHeight > spaceBelow;
+          if (notSpaceAvailableInBottom) {
+            // it not space available in bottom too , then return targetTop with reduce version
+            return targetTop - (containerHeight - spaceBelow);
+          } else {
+            return targetTop;
+          }
         } else {
-          return windowHeight - targetBottom <= 0
-            ? targetTop - (containerHeight - spaceBelow)
-            : targetBottom - containerHeight;
+          // top side option mind
+
+          // in resize , this check does target item is not in viewport or not for resize behavior
+          const isTargetItemNotInViewPort = viewportHeight - targetBottom <= 0;
+          if (isTargetItemNotInViewPort) {
+            // then return the targetLeft with reduce version
+            return targetTop - (containerHeight - spaceBelow);
+          } else {
+            // if target item is in viewport still , then return relative positino
+            // why i use targetBottom safely is because of targetLeft does not cover the vertical icon , i can use bottom
+            return targetBottom - containerHeight;
+          }
         }
       })();
 
