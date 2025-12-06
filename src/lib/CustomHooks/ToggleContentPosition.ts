24846941a0323303ad765a3a3b2a0ef4802bdf39
aungchanpyae33 <khantlu24@gmail.com>
2025-12-06 13:26:08 +0630

    refactor : add debounce to toggleContentPosition
    decided to add debounce because in fast resize , althought throttle is 80 , it is not enoght and running one final update is good option , to reduce double recalculatae i run as conditional with width and height check

--- src/lib/CustomHooks/ToggleContentPosition.ts
+++ src/lib/CustomHooks/ToggleContentPosition.ts
@@ -1,11 +1,108 @@
-import { RefObject, useLayoutEffect, useMemo, useState } from "react";
+import { RefObject, useLayoutEffect, useMemo, useRef, useState } from "react";
 import throttle from "../throttle";
+import debounce from "../debounce";
 
 type PositionStyle = {
   transform: string;
   height?: string;
   maxHeight?: string;
 };
+
+function calcMenuPosition(
+  parentEl: HTMLElement | null,
+  containerEl: HTMLElement | null,
+  viewportWidthStoreRef: RefObject<number | null>,
+  viewportHeightStoreRef: RefObject<number | null>
+): PositionStyle | undefined {
+  // parent is vertical icon cantainer
+  // container is content
+  if (!parentEl || !containerEl) {
+    return { transform: "translate(0px, 0px)" };
+  }
+  // always use clientWidth and clientHeight when accurate positin is needed like for toggleTip(fixed position)
+  // previouse windowidth is not accurate in windowWidth including scrollbar width and aslo inconsistent in resize
+  const viewportWidth = document.documentElement.clientWidth;
+  const viewportHeight = document.documentElement.clientHeight;
+  if (
+    viewportWidthStoreRef.current === viewportWidth &&
+    viewportHeightStoreRef.current === viewportHeight
+  ) {
+    return;
+  } else {
+    viewportWidthStoreRef.current = viewportWidth;
+    viewportHeightStoreRef.current = viewportHeight;
+  }
+  const targetRect = parentEl.getBoundingClientRect();
+  const containerRect = containerEl.getBoundingClientRect();
+  const targetTop = targetRect.top;
+  const targetLeft = targetRect.left;
+  const targetRight = targetRect.right;
+  const targetBottom = targetRect.bottom;
+  const containerHeight = containerRect.height;
+  const containerWidth = containerRect.width;
+
+  // calculate how many space is left in below when position in top is not available
+  const spaceBelow = viewportHeight - targetBottom;
+  //calculate how many space is left in right when postion is left is not available
+
+  const spaceRight = viewportWidth - targetRight;
+
+  // i wll add comment in modify , new , previouse code to explain what is it for future as in later i forget how it works in some component
+
+  // how x component is calculate
+  // it has  check -> first check is space not available in right, if not available check becomes false, then go the second option, it avalile in right( first check is false) go option to right
+  const x = (() => {
+    const notSpaceAvailableInRight = spaceRight < containerWidth;
+    if (notSpaceAvailableInRight) {
+      //in left side option mind
+
+      // in resize , this check does target item is not in viewport or not for resize behavior
+      const isTargetItemNotInViewPort = viewportWidth - targetRight <= 0;
+      if (isTargetItemNotInViewPort) {
+        // then return the targetLeft with reduce version
+        // spaceRight is necessary to maintain the correct position when target is not is the view port(ususally with -value)
+        return targetLeft - (containerWidth - spaceRight);
+      } else {
+        // if target item is in viewport still , then return relative positino
+        return targetLeft - containerWidth;
+      }
+    } else {
+      // right side option mind
+      return targetRight;
+    }
+  })();
+
+  // how y component is calculate
+  // it has  check -> first check is  space not available in bottom, if not available check becomes false, then go the second option, it avalile in bottom( first check is false) go option to bottom
+  const y = (() => {
+    const notSpaceAvailableInBelow = spaceBelow < containerHeight;
+    if (notSpaceAvailableInBelow) {
+      // top side option in mind
+
+      // in resize , this check does target item is not in viewport or not for resize behavior
+      const isTargetItemNotInViewPort = viewportHeight - targetBottom <= 0;
+      if (isTargetItemNotInViewPort) {
+        // then return the targetLeft with reduce version
+        // spaceBelow is necessary to maintain the correct position when target is not is the view port(ususally with -value)
+        return targetBottom - (containerHeight - spaceBelow);
+      } else {
+        // if target item is in viewport still , then return relative positino
+        return targetBottom - containerHeight;
+      }
+    } else {
+      //  bottom side option mind
+      return targetTop;
+    }
+  })();
+
+  const roundedY = Math.max(Math.round(y), 0);
+  const roundedX = Math.max(Math.round(x), 0);
+
+  return {
+    transform: `translate(${roundedX}px, ${roundedY}px)`,
+  };
+}
+
 export const useToggleContentPosition = ({
   parentRef,
   containerRef,
@@ -16,99 +113,48 @@ export const useToggleContentPosition = ({
   const [position, setPosition] = useState<PositionStyle>({
     transform: "translate(0px, 0px)",
   });
+  const viewportWidthStoreRef = useRef(null);
+  const viewportHeightStoreRef = useRef(null);
   // now , it will be run as sub content when available , because of that , if 3 sub content is open (currently only two) , the resize could be expensive for 3 run , so right now throttle will be add  to prevent lag in low performance machine
   const updatePositionThrottle = useMemo(
     () =>
       throttle(() => {
-        // parent is vertical icon cantainer
-        const parentEl = parentRef.current;
-        // container is content
-        const containerEl = containerRef.current;
-        if (!parentEl || !containerEl) return;
-
-        // always use clientWidth and clientHeight when accurate positin is needed like for toggleTip(fixed position)
-        // previouse windowidth is not accurate in windowWidth including scrollbar width and aslo inconsistent in resize
-        const viewportWidth = document.documentElement.clientWidth;
-        const viewportHeight = document.documentElement.clientHeight;
-        const targetRect = parentEl.getBoundingClientRect();
-        const containerRect = containerEl.getBoundingClientRect();
-        const targetTop = targetRect.top;
-        const targetLeft = targetRect.left;
-        const targetRight = targetRect.right;
-        const targetBottom = targetRect.bottom;
-        const containerHeight = containerRect.height;
-        const containerWidth = containerRect.width;
-
-        // calculate how many space is left in below when position in top is not available
-        const spaceBelow = viewportHeight - targetBottom;
-        //calculate how many space is left in right when postion is left is not available
-
-        const spaceRight = viewportWidth - targetRight;
-
-        // i wll add comment in modify , new , previouse code to explain what is it for future as in later i forget how it works in some component
-
-        // how x component is calculate
-        // it has  check -> first check is space not available in right, if not available check becomes false, then go the second option, it avalile in right( first check is false) go option to right
-        const x = (() => {
-          const notSpaceAvailableInRight = spaceRight < containerWidth;
-          if (notSpaceAvailableInRight) {
-            //in left side option mind
-
-            // in resize , this check does target item is not in viewport or not for resize behavior
-            const isTargetItemNotInViewPort = viewportWidth - targetRight <= 0;
-            if (isTargetItemNotInViewPort) {
-              // then return the targetLeft with reduce version
-              // spaceRight is necessary to maintain the correct position when target is not is the view port(ususally with -value)
-              return targetLeft - (containerWidth - spaceRight);
-            } else {
-              // if target item is in viewport still , then return relative positino
-              return targetLeft - containerWidth;
-            }
-          } else {
-            // right side option mind
-            return targetRight;
-          }
-        })();
-
-        // how y component is calculate
-        // it has  check -> first check is  space not available in bottom, if not available check becomes false, then go the second option, it avalile in bottom( first check is false) go option to bottom
-        const y = (() => {
-          const notSpaceAvailableInBelow = spaceBelow < containerHeight;
-          if (notSpaceAvailableInBelow) {
-            // top side option in mind
-
-            // in resize , this check does target item is not in viewport or not for resize behavior
-            const isTargetItemNotInViewPort =
-              viewportHeight - targetBottom <= 0;
-            if (isTargetItemNotInViewPort) {
-              // then return the targetLeft with reduce version
-              // spaceBelow is necessary to maintain the correct position when target is not is the view port(ususally with -value)
-              return targetBottom - (containerHeight - spaceBelow);
-            } else {
-              // if target item is in viewport still , then return relative positino
-              return targetBottom - containerHeight;
-            }
-          } else {
-            //  bottom side option mind
-            return targetTop;
-          }
-        })();
-
-        const roundedY = Math.max(Math.round(y), 0);
-        const roundedX = Math.max(Math.round(x), 0);
-
-        setPosition({
-          transform: `translate(${roundedX}px, ${roundedY}px)`,
-        });
+        const newPos = calcMenuPosition(
+          parentRef.current,
+          containerRef.current,
+          viewportWidthStoreRef,
+          viewportHeightStoreRef
+        );
+        if (!newPos) return;
+        setPosition(newPos);
       }, 80),
+    [containerRef, parentRef]
+  );
+
+  const UpdatePositionDebounce = useMemo(
+    () =>
+      debounce(() => {
+        const newPos = calcMenuPosition(
+          parentRef.current,
+          containerRef.current,
+          viewportWidthStoreRef,
+          viewportHeightStoreRef
+        );
+        if (!newPos) return;
+        setPosition(newPos);
+      }, 200),
     [containerRef, parentRef]
   );
   // the reason i use use layout effect is obvious , that to run this effect before the layout pain , because i need to the content width , height , etc before paint to calculate the position of its to render
   useLayoutEffect(() => {
     updatePositionThrottle();
     window.addEventListener("resize", updatePositionThrottle);
-    return () => window.removeEventListener("resize", updatePositionThrottle);
-  }, [parentRef, containerRef, updatePositionThrottle]);
+    window.addEventListener("resize", UpdatePositionDebounce);
+    return () => {
+      window.removeEventListener("resize", updatePositionThrottle);
+      window.removeEventListener("resize", UpdatePositionDebounce);
+    };
+  }, [parentRef, containerRef, updatePositionThrottle, UpdatePositionDebounce]);
 
   return [position, setPosition];
 };
