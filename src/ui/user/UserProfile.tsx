dac89e55bb6e548dd7ceeccd9c0880de4f280d02
aungchanpyae33 <khantlu24@gmail.com>
2025-12-21 15:48:23 +0630

    refactor: replace button with MoreOption component

--- src/ui/user/UserProfile.tsx
+++ src/ui/user/UserProfile.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { ReactNode, useRef, useState } from "react";
+import { ReactNode } from "react";
 import { clsx } from "clsx";
-import CloseFunctoion from "@/lib/CloseFunction";
-import OutterClick from "@/lib/OutterClick";
+import MoreOptionContext from "../trackComponent/MoreOptionContext";
+import MoreOption from "../trackComponent/MoreOption";
 
 function UserProfile({
   name,
@@ -12,37 +12,29 @@ function UserProfile({
   children: ReactNode;
 }) {
   const userName = name && name.slice(0, 1);
-  const [userOpen, setUserOpen] = useState(false);
-  const buttonRef = useRef<HTMLButtonElement | null>(null);
-  CloseFunctoion(userOpen, setUserOpen, buttonRef, false);
-  OutterClick(userOpen, setUserOpen, buttonRef);
   return (
-    <div className=" relative z-10">
-      <button
-        ref={buttonRef}
-        className="bg-[#222222] hover:bg-[#333333] w-[40px] h-[40px] rounded-full flex items-center justify-center"
-        onClick={() => setUserOpen(!userOpen)}
-      >
-        {userName}
-      </button>
-      <div
-        className={clsx(
-          "bg-[#222222]  absolute -bottom-4 border-opacity-15 border border-neutral-200 translate-y-full rounded-sm flex flex-col   right-0 w-[250px]",
-          {
-            hidden: !userOpen,
-            block: userOpen,
+    <div className=" relative  z-10">
+      <MoreOptionContext>
+        <MoreOption
+          triggerEl={
+            <div className=" size-[45px] flex bg-[#333333] items-center justify-center rounded-full">
+              {userName}
+            </div>
           }
-        )}
-      >
-        <div className="p-2 flex  gap-x-3  border-b-[1px] border-gray-700">
-          <div>icon</div>
-          <div className="truncate">
-            <div className=" truncate select-all">{name}</div>
-            <div className=" select-all truncate">@aungchanpyae3304</div>
-          </div>
-        </div>
-        <div onClick={() => setUserOpen(false)}>{children}</div>
-      </div>
+          targetElement={
+            <div className={clsx("")}>
+              <div className="p-2 flex  gap-x-3  border-b-[1px] border-gray-700">
+                <div>icon</div>
+                <div className="truncate">
+                  <div className=" truncate select-all">{name}</div>
+                  <div className=" select-all truncate">@aungchanpyae3304</div>
+                </div>
+              </div>
+              {children}
+            </div>
+          }
+        />
+      </MoreOptionContext>
     </div>
   );
 }
